df0ca3a07fdf66236ba9a5e35c38e4fcf0e9e134
gs.piit33 <gs.piit33@kyowon.co.kr>
2022-08-03 20:51:34 +0900

    fix: refactor glob import syntax

--- src/validate/index.js
+++ src/validate/index.js
@@ -1,18 +1,26 @@
+import { warn } from 'vue';
 import { defineRule } from 'vee-validate';
-import { mapKeys, snakeCase } from 'lodash-es';
+import { snakeCase } from 'lodash-es';
 import i18n from '../i18n';
 
 export function defineRules() {
-  mapKeys(
-    import.meta.globEager('./rules/*.js'),
-    (module, key) => {
-      const matched = key.match(/\/(\w+)\.js$/);
-      const { validator, message } = module.default;
+  const imported = import.meta.globEager('./rules/*.js');
+  const keys = Object.keys(imported);
 
-      defineRule(
-        snakeCase(matched[1]),
-        (...args) => validator(...args) || i18n.t(message, [args[2].field, ...args[1]]),
-      );
-    },
-  );
+  keys.forEach((key) => {
+    const matched = key.match(/\/(\w+)\.js$/);
+
+    if (!matched) {
+      warn(`Invalid rule file, could not parse "${key}"`);
+      return;
+    }
+
+    const ruleName = snakeCase(matched[1]);
+    const { validator, message } = imported[key].default;
+
+    defineRule(
+      ruleName,
+      (...args) => validator(...args) || i18n.t(message, [args[2].field, ...args[1]]),
+    );
+  });
 }
