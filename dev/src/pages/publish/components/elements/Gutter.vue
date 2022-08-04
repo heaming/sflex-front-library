de6af19f91b99a7d749c2699f8d123802f802080
손상훈 <gs.piit91@kyowon.co.kr>
2022-08-04 16:59:32 +0900

    gutter 가이드 추가

--- dev/src/pages/publish/components/elements/Gutter.vue
+++ dev/src/pages/publish/components/elements/Gutter.vue
@@ -5,7 +5,7 @@
     </h2>
     <p class="kw-guide-description">
       Gutter is used to easily define the 'margin' and 'padding' between each component.
-      <br>It is provided in the form of a 'class'.10px, 20px, 30px ... 500px
+      <br>It is provided in the form of a 'class'. 1px, 2px, 3px ... 500px
     </p>
     <div class="kw-guide-section">
       <h3
@@ -17,9 +17,6 @@
       <q-markup-table class="kw-guide-table">
         <thead>
           <tr>
-            <th>
-              Prefix
-            </th>
             <th>
               padding/margin
             </th>
@@ -37,19 +34,28 @@
         <tbody>
           <tr>
             <td>
-              kw-
+              <span style="color: #999;">[</span> p <span style="color: #999;">|</span>
+              m <span style="color: #999;">]</span><br>
+              <span style="color: #999;">
+                p = padding<br>m = margin
+              </span>
             </td>
             <td>
-              [ p | m ]
+              <span style="color: #999;">[</span> t <span style="color: #999;">|</span> r
+              <span style="color: #999;">|</span> b <span style="color: #999;">|</span> l
+              <span style="color: #999;">|</span> a <span style="color: #999;">|</span> x
+              <span style="color: #999;">|</span> y <span style="color: #999;">]</span><br>
+              <span style="color: #999;">
+                t = top<br>r = right<br>b = bottom<br>l = left<br>x = horizontal<br>y = vertical<br>
+              </span>
             </td>
             <td>
-              [ t | r | b | l | a | x | y ]-
+              <span style="color: #999;">[</span> 1 <span style="color: #999;">|</span> 2
+              <span style="color: #999;">|</span> ... <span style="color: #999;">|</span> 500
+              <span style="color: #999;">]</span>
             </td>
             <td>
-              [ 1 | 2 | ... | 50 ]
-            </td>
-            <td>
-              kw-mr-3, kw-pa-2
+              mr30, pa20
             </td>
           </tr>
         </tbody>
@@ -63,17 +69,17 @@
         example
       </h3>
       <p class="kw-guide-description">
-        kw-mr-3(margin-right:30px), kw-pa-2(padding:20px)
+        mr30(margin-right:30px), pa20(padding:20px)
       </p>
       <q-card>
         <div class="kw-guide-example">
-          <div class="gutter-exam-el kw-mr-3">
-            <div class="kw-pa-2">
+          <div class="gutter-exam-el mr30">
+            <div class="pa20">
               200px * 100px
             </div>
           </div>
           <div class="gutter-exam-el">
-            <div class="kw-pa-2">
+            <div class="pa20">
               200px * 100px
             </div>
           </div>
@@ -100,13 +106,13 @@ const vue = 'vue';
 
 const testCode = `
 <div class="kw-guide-example">
-  <div class="gutter-exam-el kw-mr-3">
-    <div class="kw-pa-2">
+  <div class="gutter-exam-el mr30">
+    <div class="pa20">
       200px * 100px
     </div>
   </div>
   <div class="gutter-exam-el">
-    <div class="kw-pa-2">
+    <div class="pa20">
       200px * 100px
     </div>
   </div>
