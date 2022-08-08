9b5280867657e2ae7564ac53928e5cf8f39440db
gs.piit33 <gs.piit33@kyowon.co.kr>
2022-08-08 17:54:00 +0900

    add component GuideCodeView

---
+++ dev/src/components/guide/GuideCodeView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="kw-guide-codeButton">
+    <button @click="active = !active">
+      Show code &darr;
+    </button>
+  </div>
+  <div
+    class="kw-guide-code"
+    :class="{'active': active}"
+  >
+    <HighCode
+      :code-value="codeValue"
+      :lang="lang"
+      theme="light"
+      width="100%"
+      height="auto"
+    />
+  </div>
+</template>
+
+<script setup>
+import { HighCode } from 'vue-highlight-code';
+import 'vue-highlight-code/dist/style.css';
+
+defineProps({
+  lang: {
+    type: String,
+    default: 'vue',
+  },
+  codeValue: {
+    type: String,
+    default: '',
+  },
+});
+
+const active = ref(false);
+</script>
+
+<style lang="scss" scoped>
+.kw-guide-code {
+  display: none;
+
+  &.active {
+    display: block;
+  }
+}
+</style>
---
+++ dev/src/components/index.js
@@ -0,0 +1,1 @@
+export { default as GuideCodeView } from './guide/GuideCodeView.vue';
