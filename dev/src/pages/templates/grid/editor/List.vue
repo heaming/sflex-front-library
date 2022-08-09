1e378d9661f9f60af649828d849a8e3e5385e97e
gs.piit33 <gs.piit33@kyowon.co.kr>
2022-08-09 18:13:27 +0900

    add grid templates

---
+++ dev/src/pages/templates/grid/editor/List.vue
@@ -0,0 +1,190 @@
+<template>
+  <kw-page class="kw-guide">
+    <h2 class="kw-guide-h2">
+      List (Dropdown)
+    </h2>
+    <q-card>
+      <kw-action-bar>
+        <kw-btn>
+          <kw-btn
+            label="선택된 행 값 확인"
+            @click="checkCurrentRowValue"
+          />
+        </kw-btn>
+      </kw-action-bar>
+      <kw-grid
+        ref="grdRef"
+        :visible-rows="3"
+        @init="initGrd"
+      />
+
+      <guide-code-view
+        :code-value="sampleCode"
+        lang="js"
+      />
+    </q-card>
+  </kw-page>
+</template>
+
+<script setup>
+import { alert, gridUtil } from '~lib';
+
+const grdRef = ref();
+
+function initGrd(data, view) {
+  const options = [
+    { codeId: 'A', codeName: 'Name of A' },
+    { codeId: 'B', codeName: 'Name of B' },
+    { codeId: 'C', codeName: 'Name of C' },
+    { codeId: 'D', codeName: 'Name of D' },
+  ];
+
+  const fields = [
+    {
+      fieldName: 'list01',
+      dataType: 'text', // default value, 생략 가능
+    },
+    {
+      fieldName: 'list02',
+    },
+    {
+      fieldName: 'list03',
+    },
+  ];
+
+  const columns = [
+    {
+      fieldName: 'list01',
+      header: 'Default',
+      options,
+      editor: { type: 'list' },
+    },
+    {
+      fieldName: 'list02',
+      header: 'Custom (optionValue, optionLabel, firstOption)',
+      options: options.map((v) => ({ value: v.codeId, label: v.codeName })),
+      optionValue: 'value',
+      optionLabel: 'label',
+      editor: { type: 'list' },
+      firstOption: 'select', // preset: 'all', 'select'
+      firstOptionValue: '', // default value, 생략 가능
+      firstOptionLabel: '선택', // default value, 생략 가능
+    },
+    {
+      fieldName: 'list03',
+      header: 'Custom (displayCallback)',
+      options,
+      editor: { type: 'list' },
+      displayCallback(g, index, value) {
+        const { values, labels } = g.columnByName(index.column);
+        const i = values.findIndex((v) => v === value);
+        return i > -1 ? ` [${values[i]}] ${labels[i]}` : '';
+      },
+    },
+  ];
+
+  data.setFields(fields);
+  view.setColumns(columns);
+  view.editOptions.editable = true;
+
+  data.setRows([
+    {
+      list01: '',
+      list02: '',
+      list03: '',
+    },
+    {
+      list01: 'A',
+      list02: 'A',
+      list03: 'A',
+    },
+    {
+      list01: 'B',
+      list02: 'B',
+      list03: 'B',
+    },
+  ]);
+}
+
+function checkCurrentRowValue() {
+  const view = grdRef.value.getView();
+  const rowValue = gridUtil.getSelectedRowValues(view)[0] || {};
+
+  alert(JSON.stringify(rowValue, null, '\t'));
+}
+
+const sampleCode = `
+  const options = [
+    { codeId: 'A', codeName: 'Name of A' },
+    { codeId: 'B', codeName: 'Name of B' },
+    { codeId: 'C', codeName: 'Name of C' },
+    { codeId: 'D', codeName: 'Name of D' },
+  ];
+
+  const fields = [
+    {
+      fieldName: 'list01',
+      dataType: 'text', // default value, 생략 가능
+    },
+    {
+      fieldName: 'list02',
+    },
+    {
+      fieldName: 'list03',
+    },
+  ];
+
+  const columns = [
+    {
+      fieldName: 'list01',
+      header: 'Default',
+      options,
+      editor: { type: 'list' },
+    },
+    {
+      fieldName: 'list02',
+      header: 'Custom (optionValue, optionLabel, firstOption)',
+      options: options.map((v) => ({ value: v.codeId, label: v.codeName })),
+      optionValue: 'value',
+      optionLabel: 'label',
+      editor: { type: 'list' },
+      firstOption: 'select', // preset: 'all', 'select'
+      firstOptionValue: '', // default value, 생략 가능
+      firstOptionLabel: '선택', // default value, 생략 가능
+    },
+    {
+      fieldName: 'list03',
+      header: 'Custom (displayCallback)',
+      options,
+      editor: { type: 'list' },
+      displayCallback(g, index, value) {
+        const { values, labels } = g.columnByName(index.column);
+        const i = values.findIndex((v) => v === value);
+        return i > -1 ? \`[\${values[i]}] \${labels[i]}\` : '';
+      },
+    },
+  ];
+
+  data.setFields(fields);
+  view.setColumns(columns);
+  view.editOptions.editable = true;
+
+  data.setRows([
+    {
+      list01: '',
+      list02: '',
+      list03: '',
+    },
+    {
+      list01: 'A',
+      list02: 'A',
+      list03: 'A',
+    },
+    {
+      list01: 'B',
+      list02: 'B',
+      list03: 'B',
+    },
+  ]);
+`;
+</script>
---
+++ dev/src/pages/templates/grid/editor/Text.vue
@@ -0,0 +1,181 @@
+<template>
+  <kw-page class="kw-guide">
+    <h2 class="kw-guide-h2">
+      Text
+    </h2>
+    <q-card>
+      <kw-action-bar>
+        <kw-btn>
+          <kw-btn
+            label="선택된 행 값 확인"
+            @click="checkCurrentRowValue"
+          />
+        </kw-btn>
+      </kw-action-bar>
+      <kw-grid
+        ref="grdRef"
+        :visible-rows="3"
+        @init="initGrd"
+      />
+
+      <guide-code-view
+        :code-value="sampleCode"
+        lang="js"
+      />
+    </q-card>
+  </kw-page>
+</template>
+
+<script setup>
+import { alert, gridUtil } from '~lib';
+
+const grdRef = ref();
+
+function initGrd(data, view) {
+  const fields = [
+    {
+      fieldName: 'text01',
+      dataType: 'text', // default value, 생략 가능
+    },
+    {
+      fieldName: 'text02',
+    },
+    {
+      fieldName: 'text03',
+    },
+    {
+      fieldName: 'text04',
+    },
+  ];
+
+  const columns = [
+    {
+      fieldName: 'text01',
+      header: 'Default',
+    },
+    {
+      fieldName: 'text02',
+      header: 'Custom (align class)',
+      styleName: 'text-center', // text-left | text-center | text-right
+    },
+    {
+      fieldName: 'text03',
+      header: 'Custom (maxLength)',
+      editor: {
+        type: 'text', // default value, 생략 가능
+        maxLength: 100,
+      },
+    },
+    {
+      fieldName: 'text04',
+      header: 'Multiline',
+      editor: {
+        type: 'multiline',
+      },
+    },
+  ];
+
+  data.setFields(fields);
+  view.setColumns(columns);
+  view.editOptions.editable = true;
+
+  data.setRows([
+    {
+      text01: '기본 텍스트 필드 입니다',
+      text02: '중앙 정렬 입니다. 사용 가능 클래스: text-left, text-center, text-right',
+      text03: '100Byte까지 입력 가능합니다. 한글은 3Byte 입니다.',
+      text04: '멀티라인으로 입력이 가능한 컬럼, \n줄바꿈이 가능합니다',
+    },
+    {
+      text01: '기본 텍스트 필드 입니다',
+      text02: '중앙 정렬 입니다.',
+      text03: '한글은 3Byte 입니다.',
+      text04: '줄바꿈이 가능합니다 \n줄바꿈이 가능합니다',
+    },
+    {
+      text01: '기본 텍스트 필드 입니다',
+      text02: '중앙 정렬 입니다.',
+      text03: '일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼',
+      text04: '줄바꿈이 가능합니다 \n줄바꿈이 가능합니다',
+    },
+  ]);
+}
+
+function checkCurrentRowValue() {
+  const view = grdRef.value.getView();
+  const rowValue = gridUtil.getSelectedRowValues(view)[0] || {};
+
+  alert(JSON.stringify(rowValue, null, '\t'));
+}
+
+const sampleCode = `
+  const fields = [
+    {
+      fieldName: 'text01',
+      dataType: 'text', // default value, 생략 가능
+    },
+    {
+      fieldName: 'text02',
+    },
+    {
+      fieldName: 'text03',
+    },
+    {
+      fieldName: 'text04',
+    },
+  ];
+
+  const columns = [
+    {
+      fieldName: 'text01',
+      header: 'Default',
+    },
+    {
+      fieldName: 'text02',
+      header: 'Custom (align class)',
+      styleName: 'text-center', // text-left | text-center | text-right
+    },
+    {
+      fieldName: 'text03',
+      header: 'Custom (maxLength)',
+      editor: {
+        type: 'text', // 생략 가능
+        maxLength: 100,
+      },
+    },
+    {
+      fieldName: 'text04',
+      header: 'Multiline',
+      editor: {
+        type: 'multiline',
+      },
+    },
+  ];
+
+  data.setFields(fields);
+  view.setColumns(columns);
+  view.editOptions.editable = true;
+
+  data.setRows([
+    {
+      text01: '기본 텍스트 필드 입니다',
+      text02: '중앙 정렬 입니다. 사용 가능 클래스: text-left, text-center, text-right',
+      text03: '100Byte까지 입력 가능합니다. 한글은 3Byte 입니다.',
+      text04: '멀티라인으로 입력이 가능한 컬럼, \\n줄바꿈이 가능합니다',
+    },
+    {
+      text01: '기본 텍스트 필드 입니다',
+      text02: '중앙 정렬 입니다.',
+      text03: '한글은 3Byte 입니다.',
+      text04: '줄바꿈이 가능합니다 \\n줄바꿈이 가능합니다',
+    },
+    {
+      text01: '기본 텍스트 필드 입니다',
+      text02: '중앙 정렬 입니다.',
+      text03: '일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼',
+      text04: '줄바꿈이 가능합니다 \\n줄바꿈이 가능합니다',
+    },
+  ]);
+`;
+
+</script>
