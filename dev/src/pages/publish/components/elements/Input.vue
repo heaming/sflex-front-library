05b2c4eebb4c0fa7ef219e7113aea1ece9884f82
손상훈 <gs.piit91@kyowon.co.kr>
2022-08-09 17:21:18 +0900

    변경사항 반영

---
+++ dev/src/pages/publish/components/elements/Input.vue
@@ -0,0 +1,264 @@
+<template>
+  <kw-page class="kw-guide">
+    <h2 class="kw-guide-h2">
+      Input
+    </h2>
+    <p class="kw-guide-description">
+      default-height: 40px / searchform - height: 32px
+    </p>
+    <div class="kw-guide-section">
+      <h3
+        id="summary"
+        class="kw-guide-title"
+      >
+        summary
+      </h3>
+      <q-markup-table class="kw-guide-table">
+        <thead>
+          <tr>
+            <th>
+              the Element(tag's name)
+            </th>
+            <th>
+              attribute(class)
+            </th>
+            <th>
+              Description
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              kw-form-item (sample)
+            </td>
+            <td>
+              essential - class (sample)
+            </td>
+            <td>
+              add the class "essential" in the tag of kw-form-item. (sample)
+            </td>
+          </tr>
+        </tbody>
+      </q-markup-table>
+    </div>
+    <div class="kw-guide-section">
+      <h3
+        id="default"
+        class="kw-guide-title"
+      >
+        default
+      </h3>
+      <p class="kw-guide-description">
+        input default(height:40px)
+      </p>
+      <q-card>
+        <div class="kw-guide-example">
+          <kw-form>
+            <kw-form-row>
+              <kw-form-item label="label">
+                <kw-input
+                  name="input"
+                  rules="required"
+                  placeholder="입력해주세요"
+                />
+              </kw-form-item>
+            </kw-form-row>
+          </kw-form>
+        </div>
+        <div class="kw-guide-code">
+          <HighCode
+            :code-value="inputCode"
+            :lang="vue"
+            theme="light"
+            width="100%"
+            height="auto"
+          />
+        </div>
+      </q-card>
+    </div>
+    <div class="kw-guide-section">
+      <h3
+        id="essential"
+        class="kw-guide-title"
+      >
+        essential
+      </h3>
+      <p class="kw-guide-description">
+        add <b> "essential" </b> of the classes in <b>
+          &lt;kw-form-item&gt; </b>
+        when it needs to be essential to
+        input
+      </p>
+      <q-card>
+        <div class="kw-guide-example">
+          <kw-form>
+            <kw-form-row>
+              <kw-form-item
+                label="label"
+                class="essential"
+              >
+                <kw-input
+                  name="input"
+                  rules="required"
+                  placeholder="입력해주세요"
+                />
+              </kw-form-item>
+            </kw-form-row>
+          </kw-form>
+        </div>
+        <div class="kw-guide-code">
+          <HighCode
+            :code-value="essentialCode"
+            :lang="vue"
+            theme="light"
+            width="100%"
+            height="auto"
+          />
+        </div>
+      </q-card>
+    </div>
+    <div class="kw-guide-section">
+      <h3
+        id="readonly"
+        class="kw-guide-title"
+      >
+        readonly
+      </h3>
+      <p class="kw-guide-description">
+        add the attribute <b>"readonly"</b> in the tag of "kw-input"
+      </p>
+      <q-card>
+        <div class="kw-guide-example">
+          <kw-form>
+            <kw-form-row>
+              <kw-form-item label="label">
+                <kw-input
+                  name="input"
+                  readonly
+                  rules="required"
+                  placeholder="입력해주세요"
+                />
+              </kw-form-item>
+            </kw-form-row>
+          </kw-form>
+        </div>
+        <div class="kw-guide-code">
+          <HighCode
+            :code-value="inputReadonlyCode"
+            :lang="vue"
+            theme="light"
+            width="100%"
+            height="auto"
+          />
+        </div>
+      </q-card>
+    </div>
+    <div class="kw-guide-section">
+      <h3
+        id="disable"
+        class="kw-guide-title"
+      >
+        disable
+      </h3>
+      <p class="kw-guide-description">
+        add the attribute <b>"disable"</b> in the tag of "kw-input"
+      </p>
+      <q-card>
+        <div>
+          <div class="kw-guide-example">
+            <kw-form>
+              <kw-form-row>
+                <kw-form-item label="label">
+                  <kw-input
+                    name="input"
+                    rules="required"
+                    placeholder="입력해주세요"
+                    disable
+                  />
+                </kw-form-item>
+              </kw-form-row>
+            </kw-form>
+          </div>
+          <div class="kw-guide-code">
+            <HighCode
+              :code-value="inputDisableCode"
+              :lang="vue"
+              theme="light"
+              width="100%"
+              height="auto"
+            />
+          </div>
+        </div>
+      </q-card>
+    </div>
+  </kw-page>
+</template>
+
+<script setup>
+import { HighCode } from 'vue-highlight-code';
+import 'vue-highlight-code/dist/style.css';
+
+const vue = 'vue';
+
+const inputCode = `
+<kw-form>
+  <kw-form-row>
+    <kw-form-item label="label">
+      <kw-input
+        name="input"
+        rules="required"
+        placeholder="입력해주세요"
+      />
+    </kw-form-item>
+  </kw-form-row>
+</kw-form>
+`;
+
+const essentialCode = `
+<kw-form>
+  <kw-form-row>
+    <kw-form-item
+      label="label"
+      class="essential"
+    >
+      <kw-input
+        name="input"
+        rules="required"
+        placeholder="입력해주세요"
+      />
+    </kw-form-item>
+  </kw-form-row>
+</kw-form>
+`;
+
+const inputReadonlyCode = `
+<kw-form>
+  <kw-form-row>
+    <kw-form-item label="label">
+      <kw-input
+        name="input"
+        readonly
+        rules="required"
+        placeholder="입력해주세요"
+      />
+    </kw-form-item>
+  </kw-form-row>
+</kw-form>
+`;
+
+const inputDisableCode = `
+<kw-form>
+  <kw-form-row>
+    <kw-form-item label="label">
+      <kw-input
+        name="input"
+        disable
+        rules="required"
+        placeholder="입력해주세요"
+      />
+    </kw-form-item>
+  </kw-form-row>
+</kw-form>
+`;
+</script>
