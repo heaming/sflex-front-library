62ad48c99c5e110fbddfd0f67256b1ada96b6358
gs.piit33 <gs.piit33@kyowon.co.kr>
2022-08-10 12:45:01 +0900

    add component GuidePopupView

---
+++ dev/src/components/guide/GuidePopupView.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-dialog
+    :model-value="true"
+    :transition-duration="0"
+    persistent
+    seamless
+    square
+    no-shake
+    no-refocus
+  >
+    <kw-popup-container>
+      <slot />
+    </kw-popup-container>
+  </q-dialog>
+</template>
+
+<script setup>
+//
+</script>
