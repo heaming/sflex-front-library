c647c8489e4d8625f9ffaf5677893154bf7c29cf
gs.piit33 <gs.piit33@kyowon.co.kr>
2022-08-18 20:16:22 +0900

    fix

--- src/components/grid/private/overrideViewEvents.js
+++ src/components/grid/private/overrideViewEvents.js
@@ -369,6 +369,10 @@ export function overrideOnCellPasting(view) {
   */
 export function overrideDataDropOptionsDragCallback(view) {
   wrapEvent(view, dataDropOptionsDragCallback, (source, sourceItems, target, targetItem) => {
+    if (!hasOriginal(view, dataDropOptionsDropCallback)) {
+      return false;
+    }
+
     if (hasOriginal(view, dataDropOptionsDragCallback)) {
       return execOriginal(view, dataDropOptionsDragCallback, source, sourceItems, target, targetItem);
     }
