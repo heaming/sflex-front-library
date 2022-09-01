de1e7147d65a14a1913d8f6d3845c7f1dff2dac8
gs.piit33 <gs.piit33@kyowon.co.kr>
2022-09-01 14:53:07 +0900

    add useMeta

---
+++ src/composables/useMeta.js
@@ -0,0 +1,36 @@
+import { PageContextKey } from '../consts/private/symbols';
+import { hasPermissionKeyInPage } from '../directives/permission';
+
+export default () => {
+  const pageCtx = inject(PageContextKey, null);
+  const { getters } = useStore();
+
+  function getUserInfo() {
+    return getters('meta/getUserInfo');
+  }
+
+  function getPageInfo() {
+    return pageCtx;
+  }
+
+  function getConfig(configurationId, fallbackValue) {
+    return getters['meta/getConfig'](configurationId)?.configurationValue ?? fallbackValue;
+  }
+
+  function hasPermission(permissionKey) {
+    return hasPermissionKeyInPage(permissionKey, pageCtx);
+  }
+
+  function hasRoleNickName(roleNickName) {
+    const { roles } = getUserInfo();
+    return roles.findIndex((v) => v.roleNickName === roleNickName) > -1;
+  }
+
+  return {
+    getUserInfo,
+    getPageInfo,
+    getConfig,
+    hasPermission,
+    hasRoleNickName,
+  };
+};
