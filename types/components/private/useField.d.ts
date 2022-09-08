1cb65f5595ecba417f6998e4f11d190eab89c5e9
gs.piit33 <gs.piit33@kyowon.co.kr>
2022-09-08 16:18:32 +0900

    WIP: write typings

---
+++ types/components/private/useField.d.ts
@@ -0,0 +1,97 @@
+export interface UseFieldProps {
+  /**
+   * Form에 등록될 때 사용될 key 값으로 범위 내에서 유일한 값이어야 한다
+   */
+  name?: string;
+
+  /**
+   * 유효성 검사시 표시할 label
+   */
+  label?: string;
+
+  /**
+   * 에러 상태로 지정하는 커스텀 플래그
+   * (필요한 경우에만 사용한다)
+   */
+  error?: boolean;
+
+  /**
+   * 에러 상태일 시 표시할 커스텀 메시지
+   * (필요한 경우에만 사용한다)
+   */
+  errorMessage?: string;
+
+  /**
+   * 적용할 유효성 규칙
+   */
+  rules?: string | Object | Function;
+
+  /**
+   * 적용된 규칙에 대한 커스텀 메시지
+   */
+  customMessages?: Object;
+
+  /**
+   * onMounted 시에 유효성 검사 수행할지 여부
+   */
+  validateOnMount?: boolean;
+
+  /**
+   * 값이 변경될 시에 유효성 검사 수행할지 여부
+   * @defaultValue `true`
+   */
+  validateOnValueUpdate?: boolean;
+
+  /**
+   * 상위 Observer, Form에서 isModified 수행 시 제외할지 여부
+   */
+  ignoreOnModified?: boolean;
+
+  /**
+   * 상위 Observer, Form에서 reset 수행 시 제외할지 여부
+   */
+  ignoreOnReset?: boolean;
+}
+
+export interface UseFieldInstance {
+  /**
+   * 컴포넌트를 현재 상태로 초기화 한다
+   */
+  init: () => Promise<void>;
+
+  /**
+   * 컴포넌트를 초기 상태로 돌린다
+   * @param shouldFocus 입력 컴포넌트 포커싱 여부
+   */
+  reset: (shouldFocus?: boolean) => Promise<void>;
+
+  /**
+   * 컴포넌트 유효성 검사 상태를 초기화 한다
+   */
+  resetValidation: () => Promise<void>;
+
+  /**
+   * 컴포넌트 유효성 검사를 수행한다
+   * @param shouldFocus 유효성 검사 실패시, 입력 컴포넌트 포커싱 여부
+   */
+  validate: (shouldFocus?: boolean) => Promise<boolean>;
+
+  /**
+   * 컴포넌트의 데이터 변경 여부를 반환한다
+   */
+  isModified: () => boolean;
+
+  /**
+   * 컴포넌트의 데이터가 변경되지 않았으면, alert를 표시한다
+   * (데이터가 변경되었으면 true 반환)
+   * @param message alert에 표시할 메시지
+   */
+  alertIfIsNotModified: (message?: string) => Promise<boolean>;
+
+  /**
+   * 컴포넌트의 데이터가 변경되었으면 confirm을 표시한다
+   * (데이터가 변경되지 않았거나, confirm에서 확인 선택시 true 반환)
+   * @param message confirm에 표시할 메시지
+   */
+  confirmIfIsModified: (message?: string) => Promise<boolean>;
+}
---
+++ types/components/private/useObserver.d.ts
@@ -0,0 +1,61 @@
+export interface UseObserverChildProps {
+  /**
+   * Observer에 등록할 떄 key 값으로 사용된다.
+   */
+  name?: string;
+
+  /**
+   * Observer에서 하위 ObserverChild의 변경여부를 확인할 때, 무시할지 여부
+   */
+  ignoreOnModified?: boolean;
+
+  /**
+   * Observer에서 하위 ObserverChild를 reset할 때, 무시할지 여부
+   */
+  ignoreOnReset?: boolean;
+}
+
+export interface UseObserverProps extends UseObserverChildProps {}
+
+export interface UseObserverInstance {
+  /**
+   * 하위 Observer, Form, Grid를 현재 상태로 초기화 한다
+   */
+  init: () => Promise<void>;
+
+  /**
+   * 하위 Observer, Form, Grid를 초기 상태로 돌린다
+   * @param shouldFocus 입력 컴포넌트 포커싱 여부
+   */
+  reset: (shouldFocus?: boolean) => Promise<void>;
+
+  /**
+   * 하위 Observer, Form의 유효성 검사 상태를 초기화 한다
+   */
+  resetValidation: () => Promise<void>;
+
+  /**
+   * 하위 Observer, Form의 유효성 검사를 수행한다
+   * @param shouldFocus 유효성 검사 실패시, 입력 컴포넌트 포커싱 여부
+   */
+  validate: (shouldFocus?: boolean) => Promise<boolean>;
+
+  /**
+   * 하위 Observer, Form, Grid의 데이터 변경 여부를 반환한다
+   */
+  isModified: () => boolean;
+
+  /**
+   * 하위 Observer, Form, Grid의 데이터가 변경되지 않았으면, alert를 표시한다
+   * (데이터가 변경되었으면 true 반환)
+   * @param message alert에 표시할 메시지
+   */
+  alertIfIsNotModified: (message?: string) => Promise<boolean>;
+
+  /**
+   * 하위 Observer, Form, Grid의 데이터가 변경되었으면 confirm을 표시한다
+   * (데이터가 변경되지 않았거나, confirm에서 확인 선택시 true 반환)
+   * @param message confirm에 표시할 메시지
+   */
+  confirmIfIsModified: (message?: string) => Promise<boolean>;
+}
