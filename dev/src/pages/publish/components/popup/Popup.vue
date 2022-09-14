98f9f126bf195cd8a1073f0e2c79fca8ec2de01d
DESKTOP-M3S30PO\김지혜 <gs.piit110@kyowon.co.kr>
2022-09-14 14:25:32 +0900

    가이드 추가 | 팝업사이즈 관련 내용 추가 설명

--- dev/src/pages/publish/components/popup/Popup.vue
+++ dev/src/pages/publish/components/popup/Popup.vue
@@ -6,6 +6,9 @@
     <p class="kw-guide-description">
       It is in progress. Please wait for it to complete.
     </p>
+    <guide-history-view
+      :items="guideHistory"
+    />
     <div class="kw-guide-section">
       <h3
         id="summary"
@@ -17,30 +20,80 @@
         <thead>
           <tr>
             <th>
-              the Element(tag's name)
+              Size type
             </th>
             <th>
-              Attr or Class
+              class
             </th>
             <th>
-              Description
+              px
             </th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>
-              kw-form-item (sample)
+              sm <br>
+              md <br>
+              lg <br>
+              xl <br>
+              2xl <br>
+              3xl
             </td>
             <td>
-              essential (sample)
+              kw-popup--sm <br>
+              kw-popup--md <br>
+              kw-popup--lg <br>
+              kw-popup--xl <br>
+              kw-popup--2xl <br>
+              kw-popup--3xl
             </td>
             <td>
-              add the class "essential" in the tag of kw-form-item. (sample)
+              460px <br>
+              660px <br>
+              860px<br>
+              1060px<br>
+              1260px<br>
+              1460px
             </td>
           </tr>
         </tbody>
       </q-markup-table>
+    </div>
+    <div class="kw-guide-section">
+      <h3
+        id="default"
+        class="kw-guide-title"
+      >
+        Notice
+      </h3>
+      <ul>
+        <li>
+          <em class="text-red">[different size!]</em> <br>
+          If the popup size which is located in<em> wireframe(proposal) page is different</em>,
+          follow below step <br>
+          <br>
+          1. Check there is design screen <br>
+          2. Check there is design refrence <br>
+          <img
+            src="../../../../../assets/images/example_popup2.png"
+            width="500"
+            alt="디자인 참고"
+          > <br> <br>
+          3. If there is no infos about step 2 and 3,
+          please use the popup class which have <em>approximate value</em> size. <br>
+          <img
+            src="../../../../../assets/images/example_popup.png"
+            width="500"
+            alt="디자인 참고"
+          > <br>
+          use class="kw-popup--sm"
+        </li>
+        <li class="mt10">
+          <em class="text-red">[different button align!]</em> <br>
+          The buttons which located in bottom is <em>align center</em>
+        </li>
+      </ul>
     </div>
     <div class="kw-guide-section">
       <h3
@@ -211,4 +264,11 @@ const testCode = `
   </template>
 </kw-popup>
 `;
+
+const guideHistory = [
+  {
+    timestamp: '2022-09-14',
+    text: 'add guide information',
+  },
+];
 </script>
