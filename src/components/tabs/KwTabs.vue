cbd9bdb5806026e85cb4ec3f537abd94d9ee69d8
gs.piit33 <gs.piit33@kyowon.co.kr>
2022-09-15 07:47:24 +0900

    fix: set inheritAttrs false and declare props to use

--- src/components/tabs/KwTabs.vue
+++ src/components/tabs/KwTabs.vue
@@ -1,25 +1,41 @@
 <template>
   <q-tabs
     class="kw-tabs"
+    v-bind="styleClassAttrs"
+    :model-value="modelValue"
     :align="align"
+    @update:model-value="$emit('update:modelValue', $event)"
   >
     <slot />
   </q-tabs>
 </template>
 
 <script>
+import useInheritAttrs from '../../composables/private/useInheritAttrs';
+
 export default {
   name: 'KwTabs',
+  inheritAttrs: false,
 
   props: {
+    modelValue: {
+      type: [Number, String],
+      default: undefined,
+    },
     align: {
       type: String,
       default: 'left',
     },
   },
 
-  setup() {
+  emits: [
+    'update:modelValue',
+  ],
 
+  setup() {
+    return {
+      ...useInheritAttrs(),
+    };
   },
 };
 </script>
