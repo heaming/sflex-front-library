c441a1b118decb29f9c3eef0a8235b81f05657d4
gs.piit33 <gs.piit33@kyowon.co.kr>
2022-09-15 13:28:09 +0900

    fix: set inheritAttrs value to false, declare props to use

--- src/components/chip/KwChip.vue
+++ src/components/chip/KwChip.vue
@@ -1,18 +1,68 @@
 <template>
   <q-chip
     class="kw-chip"
+    :class="chipClass"
+    v-bind="styleClassAttrs"
+    :label="label"
+    :icon="icon"
+    :icon-right="iconRight"
+    :color="color"
+    :disable="disable"
     :ripple="false"
+    outline
+    @click="onClick"
   >
     <slot />
   </q-chip>
 </template>
 
 <script>
+import useInheritAttrs from '../../composables/private/useInheritAttrs';
 
 export default {
   name: 'KwChip',
-  setup() {
+  inheritAttrs: false,
 
+  props: {
+    label: {
+      type: String,
+      default: undefined,
+    },
+    icon: {
+      type: String,
+      default: undefined,
+    },
+    iconRight: {
+      type: String,
+      default: undefined,
+    },
+    color: {
+      type: String,
+      default: undefined,
+    },
+    clickable: {
+      type: Boolean,
+      default: false,
+    },
+    disable: {
+      type: Boolean,
+      default: false,
+    },
+    onClick: {
+      type: Function,
+      default: undefined,
+    },
+  },
+
+  setup(props) {
+    const chipClass = computed(() => [
+      props.clickable && 'cursor-pointer',
+    ]);
+
+    return {
+      ...useInheritAttrs(),
+      chipClass,
+    };
   },
 };
 </script>
