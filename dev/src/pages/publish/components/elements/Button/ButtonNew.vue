4157c55a5eb52045ba16cb988135c49180d50fc1
DESKTOP-M3S30PO\김지혜 <gs.piit110@kyowon.co.kr>
2022-09-15 12:59:07 +0900

    아이콘 버튼 가이드 파일명 수정 및 추가

--- dev/src/pages/publish/components/elements/Button/ButtonNew.vue
+++ dev/src/pages/publish/components/elements/Button/ButtonNew.vue
@@ -24,7 +24,7 @@
               For what
             </th>
             <th>
-              class
+              attribute
             </th>
             <th>
               Description
@@ -37,31 +37,12 @@
               height
             </td>
             <td style="width: 33.33%;">
-              Default<br>
-              kw-btn--h40<br>
-              kw-btn--h36<br>
-              kw-btn--h32<br>
-              kw-btn--h24
+              <br>
+              dense
             </td>
             <td style="width: 33.33%;">
               height: 42px<br>
-              height: 40px<br>
-              height: 36px<br>
-              height: 32px<br>
-              height: 24px
-            </td>
-          </tr>
-          <tr>
-            <td>
-              width
-            </td>
-            <td>
-              Default<br>
-              w1, w2, w3...w1000
-            </td>
-            <td>
-              width : auto<br>
-              If you need a specific width rather than 'auto', use a helper class. px
+              height: 32px
             </td>
           </tr>
           <tr>
@@ -69,22 +50,16 @@
               color or type
             </td>
             <td>
-              Default<br>
-              kw-btn--negative<br>
-              kw-btn--line-black<br>
-              kw-btn--line-blue<br>
-              kw-btn--gray<br>
-              kw-btn--line-gray<br>
-              kw-btn--underline
+              primary<br>
+              negative<br>
+              secondary<br>
+              table-action<br>
             </td>
             <td>
-              Primary color : #2f8af3<br>
-              Negative gray type : #777777<br>
-              Black line type : line color #222222<br>
-              Blue line type : line color #2f8af3(Primary)<br>
-              Gray color : #555555<br>
-              Gray line type : line color #cccccc<br>
-              Simple link type or with icon
+              bg: #2f8af3 / text: #fff<br>
+              bg: #777777(black3) / text: #fff<br>
+              border: #666(black-btn-line) / bg: #fff / text: #555(black3)<br>
+              border: #ddd(line-line) / bg: #f5f5f5 / text: #555(black3)
             </td>
           </tr>
         </tbody>
@@ -167,7 +142,49 @@
           />
         </div>
         <guide-code-view
-          :code-value="defaultCode"
+          :code-value="denseCode"
+          lang="vue"
+        />
+      </q-card>
+    </div>
+    <div class="kw-guide-section">
+      <h3
+        id="Form"
+        class="kw-guide-title"
+      >
+        Form type
+      </h3>
+      <p class="kw-guide-description">
+        (Default) height 40px / padding 8px 12px <br>
+        (Dense) height 32px / padding 8px 6px <br>
+        use with input type
+      </p>
+      <q-card>
+        <div class="kw-guide-example">
+          <kw-btn
+            form
+            label="파일첨부"
+            class="mr8"
+          />
+          <kw-btn
+            form
+            label="우편번호"
+            class="mr8"
+          />
+          <kw-btn
+            form
+            disable
+            label="우편번호"
+          />
+          <!-- form type dense size is same with default type dense so just put dense attribute-->
+          <kw-btn
+            dense
+            label="신규등록"
+            class="mr8"
+          />
+        </div>
+        <guide-code-view
+          :code-value="formBtn"
           lang="vue"
         />
       </q-card>
@@ -177,7 +194,7 @@
         id="popupBottomButton"
         class="kw-guide-title"
       >
-        Height & Color
+        Color
       </h3>
       <p class="kw-guide-description">
         40px,32px
@@ -258,28 +275,7 @@
           />
         </div>
         <guide-code-view
-          :code-value="blackLine"
-          lang="vue"
-        />
-        <div class="kw-guide-example">
-          <h4 class="mb20">
-            Form Button : different padding value (8px 12px)
-          </h4>
-          <kw-btn
-            secondary
-            form
-            label="파일첨부"
-            class="mr8"
-          />
-          <kw-btn
-            secondary
-            form
-            disable
-            label="파일첨부"
-          />
-        </div>
-        <guide-code-view
-          :code-value="formBtn"
+          :code-value="secondary"
           lang="vue"
         />
       </q-card>
@@ -297,13 +293,14 @@
       <q-card>
         <div class="kw-guide-example">
           <kw-btn
-            class="kw-btn--line-black kw-btn--h32"
             icon="plus_16"
+            dense
             label="학습자 추가"
+            class="mr8"
           />
           <kw-btn
-            class="kw-btn--line-black kw-btn--h32 ml8"
             icon="plus_16"
+            dense
             disable
             label="학습자 추가"
           />
@@ -342,12 +339,62 @@
       <q-card>
         <div class="kw-guide-example">
           <kw-btn
-            class="kw-btn--underline"
+            underline
             label="목록으로"
           />
         </div>
         <guide-code-view
           :code-value="underline"
+          lang="vue"
+        />
+      </q-card>
+    </div>
+    <div class="kw-guide-section">
+      <h3
+        id="table"
+        class="kw-guide-title"
+      >
+        table button
+      </h3>
+      <p class="kw-guide-description">
+        It used when related with grid and table component <br>
+      </p>
+      <q-card>
+        <div class="kw-guide-example">
+          <h4 class="mb20">
+            Located in table top and bottom
+          </h4>
+          <!-- <kw-btn
+            text-color="primary"
+            border-color="primary"
+            class="kw-btn--color-table-blue"
+            dense
+            label="삭제"
+          /> -->
+          <kw-btn
+            l-gray
+            label="저장"
+          />
+          <kw-btn
+            primary
+            label="저장"
+          />
+        </div>
+        <guide-code-view
+          :code-value="more"
+          lang="vue"
+        />
+        <div class="kw-guide-example">
+          <h4 class="mb20">
+            Located in table inside
+          </h4>
+          <kw-btn
+            class="kw"
+            label="더보기"
+          />
+        </div>
+        <guide-code-view
+          :code-value="moreExpand"
           lang="vue"
         />
       </q-card>
@@ -360,7 +407,7 @@
         Etc
       </h3>
       <p class="kw-guide-description">
-        button height 32px
+        table bkye
       </p>
       <q-card>
         <div class="kw-guide-example">
@@ -448,181 +495,215 @@ const dummyData = ref(true);
 const toggleSelected = ref('오늘');
 
 const defaultCode = `
-<kw-btn
-  primary
-  label="프로모션등록"
-  class="mr8"
-/>
-<kw-btn
-  negative
-  label="취소"
-  class="mr8"
-/>
-<kw-btn
-  secondary
-  label="확인"
-  class="mr8"
-/>
-<kw-btn
-  secondary
-  disable
-  label="확인"
-/>
-<!-- mr8: gutter margin-left:8px -->
-`;
+  <kw-btn
+    primary
+    label="프로모션등록"
+    class="mr8"
+  />
+  <kw-btn
+    negative
+    label="취소"
+    class="mr8"
+  />
+  <kw-btn
+    secondary
+    label="확인"
+    class="mr8"
+  />
+  <kw-btn
+    secondary
+    disable
+    label="확인"
+  />
+  <!-- mr8: gutter margin-left:8px -->
+  `;
 
-const primaryPositive = `
-<kw-btn
-  negative
-  label="취소"
-  class="mr8"
-/>
-<kw-btn
-  negative
-  dense
-  label="취소"
-  class="mr8"
-/>
-<kw-btn
-  negative
-  dense
-  disable
-  label="취소"
-/>
-`;
-
-const primaryNegative = `
-<kw-btn
-  class="kw-btn--negative"
-  label="취소"
-/>
-<kw-btn
-  class="kw-btn--negative kw-btn--h40 ml8"
-  label="취소"
-/>
-<kw-btn
-  class="kw-btn--negative kw-btn--h36 ml8"
-  label="취소"
-/>
-<kw-btn
-  class="kw-btn--negative kw-btn--h32 ml8"
-  label="취소"
-/>
-<kw-btn
-  class="kw-btn--negative kw-btn--h24 ml8"
-  label="취소"
-/>
-`;
-
-const blackLine = `
-<kw-btn
-  secondary
-  label="초기화"
-  class="mr8"
-/>
-<kw-btn
-  secondary
-  dense
-  label="초기화"
-  class="mr8"
-/>
-<kw-btn
-  secondary
-  dense
-  disable
-  label="초기화"
-/>
-`;
+const denseCode = `
+  <kw-btn
+    primary
+    dense
+    label="프로모션등록"
+    class="mr8"
+  />
+  <kw-btn
+    negative
+    dense
+    label="취소"
+    class="mr8"
+  />
+  <kw-btn
+    secondary
+    dense
+    label="확인"
+    class="mr8"
+  />
+  <kw-btn
+    secondary
+    dense
+    disable
+    label="확인"
+  />
+  </div>
+  `;
 
 const formBtn = `
-<kw-btn
-  secondary
-  form
-  label="파일첨부"
-  class="mr8"
-/>
-<kw-btn
-  secondary
-  form
-  disable
-  label="파일첨부"
-/>
-`;
+  <kw-btn
+    form
+    label="파일첨부"
+    class="mr8"
+  />
+  <kw-btn
+    form
+    label="우편번호"
+    class="mr8"
+  />
+  <kw-btn
+    form
+    disable
+    label="우편번호"
+  />
+  <!-- form type dense size is same with default type dense -->
+  <kw-btn
+    dense
+    label="신규등록"
+    class="mr8"
+  />
+  `;
+
+const primaryPositive = `
+  <kw-btn
+    primary
+    label="확인"
+    class="mr8"
+  />
+  <kw-btn
+    primary
+    dense
+    label="확인"
+    class="mr8"
+  />
+  <kw-btn
+    primary
+    dense
+    disable
+    label="확인"
+  />
+  `;
+
+const primaryNegative = `
+  <kw-btn
+    negative
+    label="취소"
+    class="mr8"
+  />
+  <kw-btn
+    negative
+    dense
+    label="취소"
+    class="mr8"
+  />
+  <kw-btn
+    negative
+    dense
+    disable
+    label="취소"
+  />
+  `;
+
+const secondary = `
+  <kw-btn
+    secondary
+    label="초기화"
+    class="mr8"
+  />
+  <kw-btn
+    secondary
+    dense
+    label="초기화"
+    class="mr8"
+  />
+  <kw-btn
+    secondary
+    dense
+    disable
+    label="초기화"
+  />
+  `;
 
 const contentsAddIcon = `
-<kw-btn
-  class="kw-btn--line-black kw-btn--h32"
-  icon="plus_16"
-  label="학습자 추가"
-/>
-<kw-btn
-  class="kw-btn--line-black kw-btn--h32 ml8"
-  icon="plus_16"
-  disable
-  label="학습자 추가"
-/>
-`;
+  <kw-btn
+    class="kw-btn--line-black kw-btn--h32"
+    icon="plus_16"
+    label="학습자 추가"
+  />
+  <kw-btn
+    class="kw-btn--line-black kw-btn--h32 ml8"
+    icon="plus_16"
+    disable
+    label="학습자 추가"
+  />
+  `;
 
 const contentsDefault = `
-<kw-btn
-  class="kw-btn--contents"
-  label="수정"
-/>
-<kw-btn
-  class="kw-btn--contents ml8"
-  disable
-  label="수정"
-/>
-`;
+  <kw-btn
+    class="kw-btn--contents"
+    label="수정"
+  />
+  <kw-btn
+    class="kw-btn--contents ml8"
+    disable
+    label="수정"
+  />
+  `;
 
 const more = `
-<kw-btn
-  class="kw-btn--more"
-  label="더보기"
-/>
-`;
+  <kw-btn
+    class="kw-btn--more"
+    label="더보기"
+  />
+  `;
 
 const moreExpand = `
-<kw-btn
-  class="kw-btn--more q-btn--append"
-  label="더보기"
-/>
-`;
+  <kw-btn
+    class="kw-btn--more q-btn--append"
+    label="더보기"
+  />
+  `;
 
 const underline = `
-<kw-btn
-  class="kw-btn--underline"
-  label="목록으로"
-/>
-`;
+  <kw-btn
+    class="kw-btn--underline"
+    label="목록으로"
+  />
+  `;
 
 const toggle = `
-<kw-toggle
-  v-model="dummyData"
-  label="text"
-  :true-value="true"
-  :false-value="false"
-/>
+  <kw-toggle
+    v-model="dummyData"
+    label="text"
+    :true-value="true"
+    :false-value="false"
+  />
 
-`;
+  `;
 const toggleScript = `
-const dummyData = ref(true); // select default value (true or false)
-`;
+  const dummyData = ref(true); // select default value (true or false)
+  `;
 
 const toggleRadio = `
-<kw-btn-toggle
-  v-model="toggleSelected"
-  name="btnToggle"
-  rules="required"
-  :options="['오늘', '주간', '월간']"
-  unelevated
-  outline
-/>
-`;
+  <kw-btn-toggle
+    v-model="toggleSelected"
+    name="btnToggle"
+    rules="required"
+    :options="['오늘', '주간', '월간']"
+    unelevated
+    outline
+  />
+  `;
 
 const toggleRadioScript = `
-const toggleSelected = ref('오늘')
-`;
+  const toggleSelected = ref('오늘')
+  `;
 
 // History
 const guideHistory = [
