50ed14618f418f274d2b80c5f617a28e12f0886b
gs.piit41 <gs.piit41>
2022-09-19 17:27:14 +0900

    refactor: align-content props can use in row, form

--- src/composables/private/useFormLayout.js
+++ src/composables/private/useFormLayout.js
@@ -1,5 +1,7 @@
 import { FormLayoutContextKey } from '../../consts/private/symbols';
 
+const supportedFieldAlign = ['left', 'right', 'center'];
+
 export const useFormLayoutProps = {
   cols: {
     type: Number,
@@ -8,6 +10,12 @@ export const useFormLayoutProps = {
   labelSize: {
     type: Number,
     default: undefined,
+  },
+
+  alignContent: {
+    type: String,
+    default: undefined,
+    validate: (val) => !supportedFieldAlign.find(val),
   },
 };
 
@@ -19,17 +27,21 @@ export default () => {
 
   const defaultCols = 3;
   const defaultLabelSize = 150;
+  const defaultAlignContent = 'left';
 
   const cols = computed(() => props.cols ?? injected?.cols.value ?? defaultCols);
   const labelSize = computed(() => props.labelSize ?? injected?.labelSize.value ?? defaultLabelSize);
+  const alignContent = computed(() => props.alignContent ?? injected?.alignContent.value ?? defaultAlignContent);
 
   provide(FormLayoutContextKey, {
     cols,
     labelSize,
+    alignContent,
   });
 
   return {
     cols,
     labelSize,
+    alignContent,
   };
 };
