8d8078a6b57c0ee3f9bc6c7dc9ade18849b9f4a4
gs.piit41 <gs.piit41>
2022-09-23 15:08:28 +0900

    feat: kw-action

---
+++ src/components/action/KwAction.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="kw-action">
+    <div class="kw-action__left">
+      <slot name="left" />
+    </div>
+    <div class="kw-action__contents">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'KwAction',
+};
+</script>
