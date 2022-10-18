d467fac8581f8692799e7524a8abef34331f624e
gs.piit33 <gs.piit33@kyowon.co.kr>
2022-10-18 19:25:08 +0900

    fix

--- src/components/popup/LoadFailedPopup.vue
+++ src/components/popup/LoadFailedPopup.vue
@@ -1,21 +1,21 @@
 <template>
-  <q-card-section>
-    <div class="row justify-center">
-      <q-icon
-        class="full-width"
-        size="40px"
-        name="info_24"
-      />
-      <div class="text-center mt5">
-        <p v-if="isLocaleKo">
-          팝업을 표시하는 도중 문제가 발생했습니다.<br>
-          관리자에게 문의하시기 바랍니다.
-        </p>
-        <p v-else>
-          A problem occurred while displaying the popup.<br>
-          Please contact administrator.
-        </p>
-      </div>
+  <q-card-section
+    class="kw-popup__content row justify-center"
+  >
+    <q-icon
+      class="full-width"
+      size="40px"
+      name="info_24"
+    />
+    <div class="text-center mt5">
+      <p v-if="isLocaleKo">
+        팝업을 표시하는 도중 문제가 발생했습니다.<br>
+        관리자에게 문의하시기 바랍니다.
+      </p>
+      <p v-else>
+        A problem occurred while displaying the popup.<br>
+        Please contact administrator.
+      </p>
     </div>
   </q-card-section>
 </template>
