e379c0219b6a57303bacf96bc332fd0050d3ef29
gs.piit33 <gs.piit33@kyowon.co.kr>
2022-10-23 13:23:50 +0900

    fix

--- src/components/field/KwField.vue
+++ src/components/field/KwField.vue
@@ -37,7 +37,7 @@ export default {
   ],
 
   setup(props) {
-    const fieldCtx = useField({ bindValueKey: props.fieldKey });
+    const fieldCtx = useField();
     const { value } = fieldCtx;
 
     const fieldBinding = ref({
