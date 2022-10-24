5346adb7136debc2e49fe6a41128b874f0a18213
gs.piit33 <gs.piit33@kyowon.co.kr>
2022-10-24 14:44:08 +0900

    fix

--- src/consts/private/env.js
+++ src/consts/private/env.js
@@ -7,7 +7,7 @@ export default {
   VITE_REALGRID_LIC: env.VITE_REALGRID_LIC,
 
   // HTTP
-  VITE_HTTP_ORIGIN: env.PROD ? '/' : env.VITE_HTTP_ORIGIN,
+  VITE_HTTP_ORIGIN: env.PROD ? window.location.origin : env.VITE_HTTP_ORIGIN,
   VITE_HTTP_API_VERSION_PREFIX: env.VITE_HTTP_API_VERSION_PREFIX || '/api/v1',
   VITE_HTTP_TIMEOUT: parseInt(env.VITE_HTTP_TIMEOUT || 30000, 10),
 };
