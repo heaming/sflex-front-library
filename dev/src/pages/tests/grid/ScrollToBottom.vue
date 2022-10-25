e81e6ebb1c46a17c502735873f20d00449704366
gs.piit33 <gs.piit33@kyowon.co.kr>
2022-10-25 15:57:39 +0900

    add test

---
+++ dev/src/pages/tests/grid/ScrollToBottom.vue
@@ -0,0 +1,33 @@
+<template>
+  <kw-page>
+    <kw-grid
+      :visible-rows="6"
+      @init="initGrid"
+    />
+  </kw-page>
+</template>
+
+<script setup>
+
+function initGrid(data, view) {
+  const fields = [
+    { fieldName: 'text' },
+  ];
+
+  const columns = [
+    { fieldName: 'text' },
+  ];
+
+  data.setFields(fields);
+  view.setColumns(columns);
+
+  view.onScrollToBottom = () => {
+    console.log('onScrollToBottom');
+  };
+
+  data.setRows(
+    Array(8).fill().map((v, i) => ({ text: `test${i}` })),
+  );
+}
+
+</script>
