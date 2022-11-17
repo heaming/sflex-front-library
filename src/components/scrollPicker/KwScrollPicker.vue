99e39134d60608bb14d6a5157fb4ae098fd9caee
gs.piit33 <gs.piit33@kyowon.co.kr>
2022-11-17 10:20:13 +0900

    WIP: add KwScrollPicker

---
+++ src/components/scrollPicker/KwScrollPicker.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="kw-scroll-picker">
+    <q-list
+      class="kw-scroll-picker__options"
+      :style="optionsStyle"
+      @wheel="onWheel"
+    >
+      <q-item
+        v-for="(option, i) in options"
+        :key="i"
+        :style="getOptionStyle(i)"
+        clickable
+        tabindex="-1"
+        @click="scrollTo(option.value)"
+      >
+        <q-item-section>
+          <q-item-label>
+            {{ option.label }}
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+
+    <div
+      class="scroll-picker__highlight"
+    />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'KwScrollPicker',
+
+  props: {
+
+  },
+  emits: [
+
+  ],
+
+  setup() {
+
+  },
+};
+</script>
