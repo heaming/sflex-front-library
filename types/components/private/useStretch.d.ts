5901dc599f7a18049cf7638d36bbf40579be999b
gs.piit41 <gs.piit41>
2022-11-18 14:32:39 +0900

    feat: add UseStretch composable

---
+++ types/components/private/useStretch.d.ts
@@ -0,0 +1,11 @@
+export interface UseStretch {
+  /**
+   * When used on flexbox parent, will stretch to parent's height
+   */
+  stretch?: boolean;
+
+  /**
+   * When used on flexbox parent, will stretch to parent's width
+   */
+  grow?: boolean;
+}
