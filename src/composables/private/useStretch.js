41bf9bf78ea39b45a5a0668c25cce73e40537afd
gs.piit41 <gs.piit41>
2022-11-21 17:54:21 +0900

    fix: fix use stretch bug

--- src/composables/private/useStretch.js
+++ src/composables/private/useStretch.js
@@ -1,5 +1,6 @@
 export const useStretchProps = {
   grow: { type: Boolean, default: undefined },
+  overflow: { type: Boolean, default: undefined },
   stretch: { type: Boolean, default: undefined },
 };
 
@@ -8,6 +9,7 @@ export default () => {
 
   const stretchClasses = computed(() => ({
     grow: props.grow,
+    'shrink-off': props.overflow,
     'self-stretch': props.stretch,
   }));
 
