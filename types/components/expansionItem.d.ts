cb60a36533e3cf6d88b520babc15a86050c717c8
gs.piit41 <gs.piit41>
2022-11-24 15:29:15 +0900

    fix: add type declare

--- types/components/expansionItem.d.ts
+++ types/components/expansionItem.d.ts
@@ -1,12 +1,28 @@
 import { ComponentPublicInstance } from 'vue';
 import { GlobalComponentConstructor, QExpansionItemProps, QExpansionItemSlots } from 'quasar';
 
-type privateKey = 'dark' | 'group';
+type FallThroughProps = 'toggleAriaLabel' | 'icon' | 'label' | 'labelLines' | 'caption' | 'captionLines' | 'dense' | 'expandIcon' | 'expandedIcon' | 'expandIconClass' | 'duration' | 'headerInsetLevel' | 'contentInsetLevel' | 'expandSeparator' | 'defaultOpened' | 'expandIconToggle' | 'switchToggleSide' | 'denseToggle' | 'group' | 'popup' | 'headerStyle' | 'headerClass' | 'dark' | 'to' | 'replace' | 'exact' | 'href' | 'target' | 'activeClass' | 'exactActiveClass' | 'disable' | 'modelValue' | 'onUpdate:modelValue' | 'onShow' | 'onBeforeShow' | 'onHide' | 'onBeforeHide' | 'onAfterShow' | 'onAfterHide';
 
-interface KwExpansionItemProps extends Omit<QExpansionItemProps, privateKey> {}
+interface KwExpansionItemProps extends Pick<QExpansionItemProps, FallThroughProps> {}
 interface KwExpansionItemSlots extends QExpansionItemSlots {}
 
-export interface KwExpansionItem extends ComponentPublicInstance<KwExpansionItemProps> {}
+export interface KwExpansionItem extends ComponentPublicInstance<KwExpansionItemProps> {
+  /**
+   * Triggers component to show
+   * @param evt JS event object
+   */
+  show: (evt?: Event) => void;
+  /**
+   * Triggers component to hide
+   * @param evt JS event object
+   */
+  hide: (evt?: Event) => void;
+  /**
+   * Triggers component to toggle between show/hide
+   * @param evt JS event object
+   */
+  toggle: (evt?: Event) => void;
+}
 
 declare module '@vue/runtime-core' {
   interface GlobalComponents {
--- types/components/stepper.d.ts
+++ types/components/stepper.d.ts
@@ -1,19 +1,37 @@
-import { ComponentPublicInstance, VNode } from 'vue';
-import { GlobalComponentConstructor } from 'quasar';
+import { ComponentPublicInstance } from 'vue';
+import { GlobalComponentConstructor, QStepperNavigationProps, QStepperNavigationSlots, QStepperProps, QStepperSlots, QStepProps, QStepSlots } from 'quasar';
+
+type FallThroughStepperProps = 'dark' | 'modelValue' | 'keepAlive' | 'keepAliveInclude' | 'keepAliveExclude' | 'keepAliveMax' | 'animated' | 'infinite' | 'swipeable' | 'vertical' | 'transitionPrev' | 'transitionNext' | 'transitionDuration' | 'flat' | 'bordered' | 'alternativeLabels' | 'headerNav' | 'contracted' | 'headerClass' | 'inactiveColor' | 'inactiveIcon' | 'doneIcon' | 'doneColor' | 'activeIcon' | 'activeColor' | 'errorIcon' | 'errorColor' | 'onUpdate:modelValue' | 'onBeforeTransition' | 'onTransition';
 
 // KwStepper
-interface KwStepperProps {}
-interface KwStepperSlots {}
-export interface KwStepper extends ComponentPublicInstance<KwStepperProps> {}
+interface KwStepperProps extends Pick<QStepperProps, FallThroughStepperProps> {}
+interface KwStepperSlots extends QStepperSlots {}
+export interface KwStepper extends ComponentPublicInstance<KwStepperProps> {
+  /**
+   * Go to next panel
+   */
+  next: () => void;
+  /**
+   * Go to previous panel
+   */
+  previous: () => void;
+  /**
+   * Go to specific panel
+   * @param panelName Panel's name, which may be a String or Number; Number does not refers to panel index, but to its name, which may be an Integer
+   */
+  goTo: (panelName: string | number) => void;
+}
+
+type FallThroughStepProps = 'name' | 'disable' | 'icon' | 'color' | 'title' | 'caption' | 'prefix' | 'doneIcon' | 'doneColor' | 'activeIcon' | 'activeColor' | 'errorIcon' | 'errorColor' | 'headerNav' | 'done' | 'error';
 
 // KwStep
-interface KwStepProps {}
-interface KwStepSlots {}
+interface KwStepProps extends Pick<QStepProps, FallThroughStepProps> {}
+interface KwStepSlots extends QStepSlots {}
 export interface KwStep extends ComponentPublicInstance<KwStepProps> {}
 
 // KwStepperNavigation
-interface KwStepperNavigationProps {}
-interface KwStepperNavigationSlots {}
+interface KwStepperNavigationProps extends QStepperNavigationProps {}
+interface KwStepperNavigationSlots extends QStepperNavigationSlots {}
 export interface KwStepperNavigation extends ComponentPublicInstance<KwStepperNavigationProps> {}
 
 declare module '@vue/runtime-core' {
