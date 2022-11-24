07cbd4be5303493f43abde2765a192de93eb7fb4
gs.piit41 <gs.piit41>
2022-11-24 21:48:24 +0900

    fix: add required in mobile label.

--- src/composables/private/useFieldStyle.js
+++ src/composables/private/useFieldStyle.js
@@ -25,13 +25,32 @@ export const useFieldStyleProps = {
 };
 
 export default () => {
-  const { props } = getCurrentInstance();
+  const { props, attrs } = getCurrentInstance();
 
   const computedDense = useDense();
 
   const { stretchClass } = useStretch();
 
+  // fixme
+  const required = computed(() => {
+    if (props.required || attrs.required) {
+      return true;
+    }
+    if (props.rules) {
+      if (typeof props.rules === 'string') {
+        return props.rules.includes('required');
+      }
+      if (Array.isArray(props.rules)) {
+        return props.rules.includes('required');
+      }
+      if (typeof props.rules === 'object') {
+        return props.rules.required;
+      }
+    }
+  });
+
   const fieldClass = computed(() => ({
+    'kw-field--required': required.value,
     ...stretchClass.value,
   }));
 
