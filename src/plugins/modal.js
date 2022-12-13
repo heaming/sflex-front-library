e8fd39dde6eb19294c684407e8e8f6604ba9ee08
gs.piit33 <gs.piit33@kyowon.co.kr>
2022-12-13 14:58:50 +0900

    fix: improving way open to popup entry

--- src/plugins/modal.js
+++ src/plugins/modal.js
@@ -1,6 +1,9 @@
+import { kebabCase } from 'lodash-es';
 import { GlobalModalVmKey } from '../consts/private/symbols';
 import { addGlobalData } from '../utils/private/globalData';
 import { defineGetters } from '../utils/private/globalProperty';
+import env from '../consts/private/env';
+import { open } from '../utils/popup';
 import store from '../store';
 
 export const popups = {};
@@ -9,12 +12,24 @@ const normalizeOptions = (options) => ({
   component: options.component,
   componentProps: options.componentProps || {},
   componentResolved: false,
-  dialogProps: options.dialogProps || {},
+  window: options.window === true,
 });
 
-export async function modal(options) {
-  options = normalizeOptions(options);
+function openWindow(options) {
+  const name = options.component;
 
+  if (!popups[name]) {
+    throw new Error(`No match for ${name}`);
+  }
+
+  const kebabCased = kebabCase(name.substring(0, name.length - 1));
+  const search = new URLSearchParams(options.componentProps);
+  const url = `${env.VITE_ENTRY_POPUP_PATHNAME}#/${kebabCased}?${search}`;
+
+  return open(url);
+}
+
+async function openModal(options) {
   if (typeof options.component === 'function') {
     options.component = defineAsyncComponent(options.component);
   } else {
@@ -35,6 +50,16 @@ export async function modal(options) {
       resolve,
     });
   });
+}
+
+export async function modal(options) {
+  options = normalizeOptions(options);
+
+  if (options.window) {
+    return openWindow(options);
+  }
+
+  return openModal(options);
 }
 
 export default {
