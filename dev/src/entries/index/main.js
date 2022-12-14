7192d0b28aa03d3b61e3e5ec0cad4c985375910e
gs.piit33 <gs.piit33@kyowon.co.kr>
2022-12-14 16:15:16 +0900

    fix: refactor platform entries

--- dev/src/entries/index/main.js
+++ dev/src/entries/index/main.js
@@ -1,9 +1,12 @@
 import { createApp } from '~kw-lib';
+import { overridePlatformBySearchParams } from '~dev/utils/platform';
 import routes from '~dev/routes';
 import components from '~dev/components';
 import '~dev/css/app.scss';
 
 import App from './App.vue';
+
+overridePlatformBySearchParams();
 
 createApp(App, {
   routes,
