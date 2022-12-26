06ba63792353183ccae20392c27f0da8c66a2176
김수은 <gs.piit93@kyowon.co.kr>
2022-12-26 17:21:24 +0900

    stepper 수정

--- dev/src/pages/publish/components/temporary/PageSample07.vue
+++ dev/src/pages/publish/components/temporary/PageSample07.vue
@@ -7,89 +7,87 @@
     </template>
 
     <div class="normal-area normal-area--button-set-bottom">
-      <div class="kw-stepper-headingtext">
-        <kw-stepper
-          ref="stepper"
-          v-model="stepInitNum"
-          class="has-search"
-        >
-          <kw-step
-            :name="1"
-            title="step1"
-            :done="stepInitNum > 1"
-            prefix="1"
-          />
-          <!-- This is only Second step's content -->
-          <kw-step
-            :name="2"
-            title="step2"
-            :done="stepInitNum > 2"
-            prefix="2"
+      <kw-stepper
+        ref="stepper"
+        v-model="stepInitNum"
+        class="has-search"
+        heading-text
+      >
+        <kw-step
+          :name="1"
+          title="step1"
+          :done="stepInitNum > 1"
+          prefix="1"
+        />
+        <!-- This is only Second step's content -->
+        <kw-step
+          :name="2"
+          title="step2"
+          :done="stepInitNum > 2"
+          prefix="2"
+        />
+
+        <kw-step
+          :name="3"
+          title="step3"
+          :done="stepInitNum > 3"
+          prefix="3"
+        />
+        <kw-step-panel :name="2">
+          <kw-tabs
+            model-value="1"
+            class="my20"
           >
-            <h2>Step2 title</h2>
-            <kw-tabs
-              model-value="1"
-              class="my20"
-            >
-              <kw-tab
-                name="1"
-                label="기본속성"
-              />
-              <kw-tab
-                name="2"
-                label="관리속성"
-              />
-              <kw-tab
-                name="3"
-                label="변경이력"
-              />
-            </kw-tabs>
-            <kw-tab-panels model-value="1">
-              <kw-tab-panel name="1">
-                <div class="over-width">
-                  <kw-search>
-                    <kw-search-row>
-                      <kw-search-item label="abc">
-                        <kw-input />
-                      </kw-search-item>
-                    </kw-search-row>
-                  </kw-search>
-                </div>
-                <kw-action-top class="mt40">
-                  <span class="kw-fc--black3 text-weight-regular"> (단위:원)</span>
-                </kw-action-top>
-                <div
-                  class="grid-blank"
-                />
-              </kw-tab-panel>
-              <kw-tab-panel name="2">
-                aaaa
-              </kw-tab-panel>
-            </kw-tab-panels>
-          </kw-step>
-          <kw-step
-            :name="3"
-            title="step3"
-            :done="stepInitNum > 3"
-            prefix="3"
+            <kw-tab
+              name="1"
+              label="기본속성"
+            />
+            <kw-tab
+              name="2"
+              label="관리속성"
+            />
+            <kw-tab
+              name="3"
+              label="변경이력"
+            />
+          </kw-tabs>
+          <kw-tab-panels model-value="1">
+            <kw-tab-panel name="1">
+              <div class="over-width">
+                <kw-search>
+                  <kw-search-row>
+                    <kw-search-item label="abc">
+                      <kw-input />
+                    </kw-search-item>
+                  </kw-search-row>
+                </kw-search>
+              </div>
+              <kw-action-top class="mt40">
+                <span class="kw-fc--black3 text-weight-regular"> (단위:원)</span>
+              </kw-action-top>
+              <div class="grid-blank" />
+            </kw-tab-panel>
+            <kw-tab-panel name="2">
+              aaaa
+            </kw-tab-panel>
+          </kw-tab-panels>
+        </kw-step-panel>
+      </kw-stepper>
+      <div class="button-set--bottom">
+        <div class="button-set--bottom-left">
+          <kw-btn
+            label="이전"
           />
-        </kw-stepper>
-        <div class="button-set--bottom">
-          <div class="button-set--bottom-left">
-            <kw-btn
-              label="이전"
-            />
-          </div>
-          <div class="button-set--bottom-right">
-            <kw-btn
-              label="수정"
-            />
-            <kw-btn
-              label="목록"
-              class="ml8"
-              primary
-            />
-          </div>
+        </div>
+        <div class="button-set--bottom-right">
+          <kw-btn
+            label="수정"
+          />
+          <kw-btn
+            label="목록"
+            class="ml8"
+            primary
+          />
         </div>
       </div>
     </div>
