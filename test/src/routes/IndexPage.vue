da1d06a5266b787cc1dc1aa1ae2df8b657c4178f
gs.piit33 <gs.piit33@kyowon.co.kr>
2023-01-05 18:07:17 +0900

    fix

--- test/src/routes/IndexPage.vue
+++ test/src/routes/IndexPage.vue
@@ -1,6 +1,10 @@
 <template>
   <kw-page class="flex flex-center">
-    <!--  -->
+    <img
+      src="~~@assets/images/kstation_standard.svg"
+      width="200"
+      alt="KSS"
+    >
   </kw-page>
 </template>
 
