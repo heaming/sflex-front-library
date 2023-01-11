d3d6b32aa914d2f324b70916f26afb1ae3103985
gs.piit41 <gs.piit41>
2023-01-11 16:44:42 +0900

    feat: fix file

--- src/components/file/private/useFileHeader.js
+++ src/components/file/private/useFileHeader.js
@@ -1,21 +1,52 @@
 export const useFileHeaderProps = {
-  useHeader: { type: Boolean, default: false },
+  useHeader: { type: Boolean, default: undefined },
   scrollHorizontal: { type: Boolean, default: undefined },
-  fileNameWidth: { type: String, default: undefined },
+  nameWidth: { type: String, default: undefined },
+  appendWidth: { type: String, default: undefined },
   asideWidth: { type: String, default: undefined },
+  updateBtn: { type: [Boolean, String], default: undefined },
+  updateAllBtn: { type: [Boolean, String], default: undefined },
+  revertBtn: { type: [Boolean, String], default: undefined },
+  revertAllBtn: { type: [Boolean, String], default: undefined },
+  removeBtn: { type: [Boolean, String], default: undefined },
+  removeAllBtn: { type: [Boolean, String], default: undefined },
+  undeleteBtn: { type: [Boolean, String], default: undefined },
+  undeleteAllBtn: { type: [Boolean, String], default: undefined },
+  downloadBtn: { type: [Boolean, String], default: true },
+  downloadAllBtn: { type: [Boolean, String], default: undefined },
 };
 
 export const useFileHeaderEmits = ['scroll:file', 'scroll:header'];
 
-export default () => {
+export default (uploadCtx, ables, defaults = {}) => {
   const { props, emit, proxy } = getCurrentInstance();
+  const { t } = useI18n();
+
+  const computedProps = computed(() => ({
+    useHeader: props.useHeader ?? defaults.useHeader ?? props.multiple ?? false,
+    scrollHorizontal: props.scrollHorizontal ?? defaults.scrollHorizontal,
+    nameWidth: props.nameWidth ?? defaults.nameWidth,
+    appendWidth: props.appendWidth ?? defaults.appendWidth,
+    asideWidth: props.asideWidth ?? defaults.asideWidth,
+    updateBtn: props.updateBtn ?? defaults.updateBtn,
+    updateAllBtn: props.updateAllBtn ?? defaults.updateAllBtn,
+    revertBtn: props.revertBtn ?? defaults.revertBtn,
+    revertAllBtn: props.revertAllBtn ?? defaults.revertAllBtn,
+    removeBtn: props.removeBtn ?? defaults.removeBtn,
+    removeAllBtn: props.removeAllBtn ?? defaults.removeAllBtn,
+    undeleteBtn: props.undeleteBtn ?? defaults.undeleteBtn,
+    undeleteAllBtn: props.undeleteAllBtn ?? defaults.undeleteAllBtn,
+    downloadBtn: props.downloadBtn ?? defaults.downloadBtn,
+    downloadAllBtn: props.downloadAllBtn ?? defaults.downloadAllBtn,
+  }));
+
+  const computedUseHeader = computed(() => computedProps.value.useHeader);
 
   let ignoreSource;
 
   const fileScrollAreaRef = ref();
   const headerScrollAreaRef = ref();
 
-  // eslint-disable-next-line no-unused-vars
   function scroll(source, horizontalPosition) {
     if (ignoreSource === source) {
       ignoreSource = null;
@@ -30,37 +61,105 @@ export default () => {
       ? headerScrollAreaRef
       : fileScrollAreaRef;
 
-    areaRef.value.setScrollPosition('horizontal', horizontalPosition);
+    areaRef.value?.setScrollPosition('horizontal', horizontalPosition);
   }
 
-  const fileItemNameStyles = computed(() => {
-    let styles = '';
-    styles += props.fileNameWidth ? `min-width: ${props.fileNameWidth}; ` : '';
-    return styles;
-  });
+  const fileItemNameStyles = ref('');
+  const fileItemAppendStyles = ref('');
+  const fileItemAsideStyles = ref('');
 
-  const fileItemAsideStyles = computed(() => {
-    let styles = '';
-    styles += props.asideWidth ? `min-width: ${props.asideWidth}; ` : '';
-    return styles;
-  });
+  const getFileItemMainWidth = () => {
+    const componentRootEl = proxy.$el;
+    if (!componentRootEl) { return; }
+
+    let width = computedProps.value.nameWidth;
+
+    if (width) { return width; }
+
+    const mainEls = componentRootEl.querySelectorAll('.kw-file-item__main');
+    let maxWidth = 0;
+    mainEls.forEach((node) => {
+      if (node.getBoundingClientRect()?.width > maxWidth) {
+        maxWidth = node.getBoundingClientRect().width;
+      }
+    });
+    if (maxWidth > 0) {
+      width = `${maxWidth}px`;
+    }
+    return width;
+  };
+
+  const getFileItemAppendWidth = () => {
+    const componentRootEl = proxy.$el;
+    if (!componentRootEl) { return; }
+    const appendEls = componentRootEl.querySelectorAll('.kw-file-item__append');
+    let width = computedProps.value.appendWidth;
+    if (width) { return width; }
+    let maxWidth = 0;
+    appendEls.forEach((node) => {
+      if (node.getBoundingClientRect()?.width > maxWidth) {
+        maxWidth = node.getBoundingClientRect().width;
+      }
+    });
+    if (maxWidth > 0) {
+      width = `${maxWidth}px`;
+    }
+    return width;
+  };
+
+  const getFileItemAsideWidth = () => {
+    const componentRootEl = proxy.$el;
+    if (!componentRootEl) { return; }
+    let width = computedProps.value.asideWidth;
+    if (width) { return width; }
+    const asideEls = componentRootEl.querySelectorAll('.kw-file-item__aside');
+    let maxWidth = 0;
+    asideEls.forEach((node) => {
+      if (node.getBoundingClientRect()?.width > maxWidth) {
+        maxWidth = node.getBoundingClientRect().width;
+      }
+    });
+    if (maxWidth > 0) {
+      width = `${maxWidth}px`;
+    }
+    return width;
+  };
+
+  const updateFileItemLayout = () => {
+    if (!computedProps.value.scrollHorizontal) { return; }
+
+    const mainWidth = getFileItemMainWidth();
+    if (mainWidth) {
+      fileItemNameStyles.value = `width: ${mainWidth}; `;
+    }
+
+    const appendWidth = getFileItemAppendWidth();
+    if (appendWidth) {
+      fileItemAppendStyles.value = `width: ${appendWidth}; `;
+    }
+
+    const asideWidth = getFileItemAsideWidth();
+    if (asideWidth) {
+      fileItemAsideStyles.value = `width: ${asideWidth}; `;
+    }
+  };
 
   const onScrollFile = computed(() => (info) => {
     emit('scroll:file', info);
-    if (props.useHeader && props.scrollHorizontal) {
+    if (computedProps.value.useHeader && computedProps.value.scrollHorizontal) {
       scroll('file', info.horizontalPosition);
     }
   });
 
-  const onScrollHeader = computed(() => (props.useHeader ? (info) => {
+  const onScrollHeader = computed(() => (computedProps.value.useHeader ? (info) => {
     emit('scroll:header', info);
-    if (props.scrollHorizontal) {
+    if (computedProps.value.scrollHorizontal) {
       scroll('header', info.horizontalPosition);
     }
   } : undefined));
 
   const useHeaderFileClass = computed(() => ({
-    'kw-file--use-header': props.useHeader,
+    'kw-file--use-header': computedProps.value.useHeader,
   }));
 
   const getHeaderScrollAreaStyle = () => {
@@ -120,35 +219,171 @@ export default () => {
   const headerScrollAreaStyle = ref({});
 
   const fileScrollAreaContentsStyle = computed(() => {
-    if (!props.scrollHorizontal) {
+    if (!computedProps.value.scrollHorizontal) {
       return 'width: 100%; ';
     }
-    if (props.useHeader) {
+    if (computedProps.value.useHeader) {
       return 'padding-bottom: 10px; ';
     }
     return undefined;
   });
 
+  // header actions
+  const showUpdateBtn = computed(() => (ables.value.manualUpdate
+    && !!computedProps.value.updateBtn));
+  const updateBtnLabel = computed(() => {
+    if (!showUpdateBtn.value) {
+      return '';
+    }
+    if (typeof computedProps.value.updateBtn === 'string') {
+      return computedProps.value.updateBtn;
+    }
+    return t('FIXME', null, '동기화');
+  });
+  const showUpdateAllBtn = computed(() => (ables.value.manualUpdate
+    && !!computedProps.value.updateAllBtn));
+  const updateAllBtnLabel = computed(() => {
+    if (!showUpdateAllBtn.value) {
+      return '';
+    }
+    if (typeof computedProps.value.updateAllBtn === 'string') {
+      return computedProps.value.updateAllBtn;
+    }
+    return t('FIXME', null, '전체동기화');
+  });
+  const showRevertBtn = computed(() => (ables.value.revert
+    && !!computedProps.value.revertBtn));
+  const revertBtnLabel = computed(() => {
+    if (!showRevertBtn.value) {
+      return '';
+    }
+    if (typeof computedProps.value.revertBtn === 'string') {
+      return computedProps.value.revertBtn;
+    }
+    return t('FIXME', null, '실행취소');
+  });
+  const showRevertAllBtn = computed(() => (ables.value.revert
+    && !!computedProps.value.revertAllBtn));
+  const revertAllBtnLabel = computed(() => {
+    if (!showRevertAllBtn.value) {
+      return '';
+    }
+    if (typeof computedProps.value.revertAllBtn === 'string') {
+      return computedProps.value.revertAllBtn;
+    }
+    return t('FIXME', null, '전체실행취소');
+  });
+  const showRemoveBtn = computed(() => (ables.value.remove
+    && !!computedProps.value.removeBtn));
+  const removeBtnLabel = computed(() => {
+    if (!showRemoveBtn.value) {
+      return '';
+    }
+    if (typeof computedProps.value.removeBtn === 'string') {
+      return computedProps.value.removeBtn;
+    }
+    return t('MSG_BTN_RMV', null, '삭제');
+  });
+  const showRemoveAllBtn = computed(() => (ables.value.remove
+    && !!computedProps.value.removeAllBtn));
+  const removeAllBtnLabel = computed(() => {
+    if (!showRemoveAllBtn.value) {
+      return '';
+    }
+    if (typeof computedProps.value.removeAllBtn === 'string') {
+      return computedProps.value.removeAllBtn;
+    }
+    return t('FIXME', null, '전체삭제');
+  });
+  const showUndeleteBtn = computed(() => (ables.value.undelete
+    && !!computedProps.value.undeleteBtn));
+  const undeleteBtnLabel = computed(() => {
+    if (!showUndeleteBtn.value) {
+      return '';
+    }
+    if (typeof computedProps.value.undeleteBtn === 'string') {
+      return computedProps.value.undeleteBtn;
+    }
+    return t('FIXME', null, '삭제취소');
+  });
+  const showUndeleteAllBtn = computed(() => (ables.value.undelete
+    && !!computedProps.value.undeleteAllBtn));
+  const undeleteAllBtnLabel = computed(() => {
+    if (!showUndeleteAllBtn.value) {
+      return '';
+    }
+    if (typeof computedProps.value.undeleteAllBtn === 'string') {
+      return computedProps.value.undeleteAllBtn;
+    }
+    return t('FIXME', null, '전체삭제취소');
+  });
+  const showDownloadBtn = computed(() => (ables.value.download
+    && !!computedProps.value.downloadBtn));
+  const downloadBtnLabel = computed(() => {
+    if (!showDownloadBtn.value) {
+      return '';
+    }
+    if (typeof computedProps.value.downloadBtn === 'string') {
+      return computedProps.value.downloadBtn;
+    }
+    return t('FIXME', null, '다운로드');
+  });
+  const showDownloadAllBtn = computed(() => (ables.value.download
+    && !!computedProps.value.downloadAllBtn));
+  const downloadAllBtnLabel = computed(() => {
+    if (!showDownloadAllBtn.value) {
+      return '';
+    }
+    if (typeof computedProps.value.downloadAllBtn === 'string') {
+      return computedProps.value.downloadAllBtn;
+    }
+    return t('FIXME', null, '전체다운로드');
+  });
+
   onUpdated(() => {
     headerScrollAreaStyle.value = getHeaderScrollAreaStyle();
     placeholderStyle.value = getPlaceholderStyle();
+    updateFileItemLayout();
   });
 
   onMounted(() => {
     headerScrollAreaStyle.value = getHeaderScrollAreaStyle();
     placeholderStyle.value = getPlaceholderStyle();
+    updateFileItemLayout();
   });
 
   return {
+    computedUseHeader,
     useHeaderFileClass,
     fileScrollAreaRef,
     headerScrollAreaRef,
     fileItemNameStyles,
+    fileItemAppendStyles,
     fileItemAsideStyles,
     onScrollFile,
     onScrollHeader,
     headerScrollAreaStyle,
     placeholderStyle,
     fileScrollAreaContentsStyle,
+    showUpdateBtn,
+    updateBtnLabel,
+    showUpdateAllBtn,
+    updateAllBtnLabel,
+    showRevertBtn,
+    revertBtnLabel,
+    showRevertAllBtn,
+    revertAllBtnLabel,
+    showRemoveBtn,
+    removeBtnLabel,
+    showRemoveAllBtn,
+    removeAllBtnLabel,
+    showUndeleteBtn,
+    undeleteBtnLabel,
+    showUndeleteAllBtn,
+    undeleteAllBtnLabel,
+    showDownloadBtn,
+    downloadBtnLabel,
+    showDownloadAllBtn,
+    downloadAllBtnLabel,
   };
 };
