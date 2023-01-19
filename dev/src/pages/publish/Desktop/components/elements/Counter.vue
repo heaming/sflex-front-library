5c38cafeeffee45ecfb66bfbd1a55b955f8ec910
DESKTOP-M3S30PO\김지혜 <gs.piit110@kyowon.co.kr>
2023-01-19 13:30:17 +0900

    counter guide 추가

---
+++ dev/src/pages/publish/Desktop/components/elements/Counter.vue
@@ -0,0 +1,85 @@
+<template>
+  <kw-page class="kw-guide">
+    <h2 class="kw-guide-h2">
+      Counter
+    </h2>
+    <p class="kw-guide-description" />
+    <guide-history-view
+      :items="guideHistory"
+    />
+    <div class="kw-guide-section">
+      <h3
+        id="summary"
+        class="kw-guide-title"
+      >
+        Summary
+      </h3>
+      <q-markup-table class="kw-guide-table">
+        <thead>
+          <tr>
+            <th>
+              the Element(tag's name)
+            </th>
+            <th>
+              Attr or Class
+            </th>
+            <th>
+              Description
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              zwcm-counter
+            </td>
+            <td>
+              -
+            </td>
+            <td>
+              This is a counting component
+            </td>
+          </tr>
+        </tbody>
+      </q-markup-table>
+    </div>
+    <div class="kw-guide-section">
+      <h3
+        id="default"
+        class="kw-guide-title"
+      >
+        Default
+      </h3>
+      <p class="kw-guide-description">
+        description
+      </p>
+      <q-card>
+        <div class="kw-guide-example">
+          <img
+            src="~~dev/assets/images/example_counter.png"
+            alt="address example"
+            style="width: 15%;"
+          >
+        </div>
+        <guide-code-view
+          :code-value="counterCode"
+          lang="vue"
+        />
+      </q-card>
+    </div>
+  </kw-page>
+</template>
+
+<script setup>
+
+const counterCode = `
+ <zwcm-counter/>
+`;
+
+const guideHistory = [
+  {
+    timestamp: '2023-01-19',
+    text: 'Add new guide',
+  },
+];
+</script>
