8e0c37a8ddcbaf4d877bc1270d865e2a9bc6dd19
gs.piit41 <gs.piit41>
2023-01-20 19:25:14 +0900

    fix: Tab and Tabs

--- src/components/tabs/KwTabs.vue
+++ src/components/tabs/KwTabs.vue
@@ -1,10 +1,23 @@
 <template>
   <q-tabs
-    class="kw-tabs"
     :class="tabsClass"
     v-bind="styleClassAttrs"
     :model-value="modelValue"
-    :align="align"
+    :vertical="vertical"
+    :active-class="computedActiveClass"
+    :active-color="activeColor"
+    :active-bg-color="activeBgColor"
+    :indicator-color="indicatorColor"
+    :left-icon="leftIcon"
+    :right-icon="rightIcon"
+    :outside-arrows="outsideArrows"
+    :mobile-arrows="mobileArrows"
+    :switch-indicator="switchIndicator"
+    :narrow-indicator="narrowIndicator"
+    :inline-label="inlineLabel"
+    :no-caps="noCaps"
+    :dense="computedDense"
+    :content-class="computedContentClass"
     @update:model-value="$emit('update:modelValue', $event)"
   >
     <slot />
@@ -13,38 +26,104 @@
 
 <script>
 import useInheritAttrs from '../../composables/private/useInheritAttrs';
+import useDense, { useDenseProps } from '../../composables/private/useDense';
 
 export default {
   name: 'KwTabs',
   inheritAttrs: false,
 
   props: {
-    modelValue: {
-      type: [Number, String],
-      default: undefined,
-    },
-    align: {
-      type: String,
-      default: 'left',
-    },
-    dense: {
-      type: Boolean,
-      default: undefined,
-    },
+    color: { type: String, default: undefined },
+    bgColor: { type: String, default: undefined },
+    borderColor: { type: String, default: undefined },
+    activeBorderColor: { type: String, default: undefined },
+    activeLine: { type: String, default: 'bottom' },
+    align: { type: String, default: 'left' },
+    indicator: { type: Boolean, default: undefined },
+    noPadding: { type: Boolean, default: undefined },
+
+    // fall through props
+    ...useDenseProps,
+    modelValue: { type: [Number, String], default: undefined },
+    vertical: { type: Boolean, default: undefined },
+    activeClass: { type: String, default: undefined },
+    activeColor: { type: String, default: undefined },
+    activeBgColor: { type: String, default: undefined },
+    indicatorColor: { type: String, default: undefined },
+    leftIcon: { type: String, default: 'arrow_left' },
+    rightIcon: { type: String, default: 'arrow_right' },
+    outsideArrows: { type: Boolean, default: true },
+    mobileArrows: { type: Boolean, default: undefined },
+    switchIndicator: { type: Boolean, default: undefined },
+    narrowIndicator: { type: Boolean, default: false },
+    inlineLabel: { type: Boolean, default: undefined },
+    noCaps: { type: Boolean, default: true },
+    contentClass: { type: String, default: undefined },
   },
 
   emits: [
+    // fall through emits
     'update:modelValue',
   ],
 
   setup(props) {
-    const tabsClass = computed(() => ({
-      'kw-tabs--dense': !!props.dense,
-    }));
+    const activeLineColor = computed(() => props.activeBorderColor ?? 'black1');
+
+    const tabsClass = computed(() => {
+      const classes = {
+        'kw-tabs': true,
+        'kw-tabs--dense': props.dense,
+        'kw-tabs--show-indicator': props.indicator,
+        'kw-tabs--no-padding': props.noPadding,
+        'kw-tabs--vertical': props.vertical,
+      };
+      if (props.activeBorderColor) {
+        classes[`kw-tabs--active-border-${props.activeBorderColor}`] = true;
+      }
+      if (props.activeLine) {
+        classes[`kw-tabs--active-line-${props.activeLine}`] = true;
+        classes[`kw-tabs--active-line-${activeLineColor.value}`] = true;
+      }
+      if (props.align) {
+        classes[`kw-tabs--align-${props.align}`] = true;
+      }
+      return classes;
+    });
+
+    // notify! quasar only accept string....
+    const computedContentClass = computed(() => {
+      let classes = 'kw-tabs__content';
+      if (props.contentClass) {
+        classes += ` ${props.contentClass}`;
+      }
+      if (props.color) {
+        classes += ` text-${props.color}`;
+      }
+      if (props.bgColor) {
+        classes += ` bg-${props.bgColor}`;
+      }
+      if (props.borderColor) {
+        classes += ` border-color-${props.borderColor}`;
+      }
+      return classes;
+    });
+
+    const computedActiveClass = computed(() => {
+      let classes = 'kw-tab--active';
+      if (props.activeClass) {
+        classes += ` ${props.activeClass}`;
+      }
+      return classes;
+    });
+
+    const computedDense = useDense();
 
     return {
       ...useInheritAttrs(),
       tabsClass,
+      computedDense,
+      computedContentClass,
+      computedActiveClass,
     };
   },
 };
--- types/components/tabs.d.ts
+++ types/components/tabs.d.ts
@@ -1,18 +1,95 @@
 import { ComponentPublicInstance, VNode } from 'vue';
-import { GlobalComponentConstructor } from 'quasar';
+import { GlobalComponentConstructor, QTabsProps } from 'quasar';
+
+type FallThroughProps = 'vertical' | 'outsideArrows' | 'mobileArrows' | 'activeColor' | 'activeBgColor' | 'indicatorColor' | 'contentClass' | 'activeClass' | 'leftIcon' | 'rightIcon' | 'switchIndicator' | 'narrowIndicator' | 'inlineLabel' | 'onUpdate:modelValue';
 
 // KwTabs
-interface KwTabsProps {
+interface KwTabsProps extends Pick<QTabsProps, FallThroughProps> {
   /**
-   * 현재 설정된 값
+   * 현재 선택된 탭의 name 값을 바인딩 하는 값.
+   * @example v-model="selectedTab"
    */
   modelValue?: number | string;
 
   /**
-   * 현재 설정된 값
-   * @defaultValue `right`
+   * tab 들을 어떻게 배치할 지 지정한다.
+   *
+   * !전체적인 content 외부 스타일링을 위해 전체적인 레이아웃이 바뀌어서 커스텀 됨.
+   *
+   * @default left
    */
-  align?: 'left' | 'center' | 'right' | 'justify';
+  align?: 'left' | 'center' | 'right' | 'start' | 'end' | 'justify';
+
+  /**
+   * dense 스타일로 표시한다.
+   * font 가 작아지고, 높이가 줄어들고 기본 패딩이 사라진다.
+   *
+   * tab 모양에도 영향을 미친다.
+   *
+   * @default left
+   */
+  dense?: boolean | undefined;
+
+  /**
+   * Tabs 영역 내부에 스크롤링이 일어났을 떄, 곧 탭이 주어진 공간보다 많을 때, 좌측 스크롤 이동 버튼의 아이콘을 지정한다.
+   * 모바일에서는 터치 이벤트로 스크롤 하는 것이 기본으로, mobileArrows 없이는 표기되지 않는다.
+   *
+   * @default 'arrow_left'
+   * @see outsideArrows
+   * @see mobileArrows
+   */
+  leftIcon?: string | undefined;
+
+  /**
+   * Tabs 영역 내부에 스크롤링이 일어났을 떄, 곧 탭이 주어진 공간보다 많을 때, 우측 스크롤 이동 버튼의 아이콘을 지정한다.
+   * 모바일에서는 터치 이벤트로 스크롤 하는 것이 기본으로, mobileArrows 없이는 표기되지 않는다.
+   *
+   * @default 'arrow_right'
+   * @see outsideArrows
+   * @see mobileArrows
+   */
+  rightIcon?: string | undefined;
+
+  /**
+   * 퀘이사 기본 인디케이터를 표시한다.
+   * 현재 스타일과 상당히 조화롭지 않으니, 스타일을 대거 수정할 것이 아니면 사용하지 않도록 하자.
+   * 쓰고 싶다면 border 를 그리는 혹은 비슷하게 표시하는 관련한 스타일을 전부 제거하고 사용하자.
+   */
+  indicator?: string | undefined;
+
+  /**
+   * Tab 의 text color 를 지정한다.
+   */
+  color?: string | undefined;
+
+  /**
+   * Tab 의  background-color 를 지정한다.
+   */
+  bgColor?: string | undefined;
+
+  /**
+   * Tab 의  border-color 를 지정한다.
+   */
+  borderColor?: string | undefined;
+
+  /**
+   * 활성화 된 Tab 의  border-color 를 지정한다.
+   */
+  activeBorderColor?: string | undefined;
+
+  /**
+   * 프로젝트에서 원하는 스타일이다.
+   *
+   * 해당 위치에 있는 패녈과 선택된 탭을 연결된 느낌을 준다.
+   *
+   * @default 'bottom'
+   */
+  activeLine?: 'bottom' | 'top' | 'left' | 'right' | undefined;
+
+  /**
+   * 기본 패딩을 없엔다.
+   */
+  noPadding?: boolean | undefined;
 
   /**
    * 'v-model:modelValue'에서 값 변경을 위해 사용
