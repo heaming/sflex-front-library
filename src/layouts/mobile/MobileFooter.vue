0dfc1c93b7873b4dfbe100b12043333a6941e9bc
mingyu.kim <gs.piit166@kyowon.co.kr>
2023-03-18 17:11:11 +0900

    feat: mobile 전체메뉴 개발

--- src/layouts/mobile/MobileFooter.vue
+++ src/layouts/mobile/MobileFooter.vue
@@ -2,17 +2,61 @@
   <q-footer
     class="mobile-footer flex items-center"
     bordered
-  />
+  >
+    <kw-btn
+      borderless
+      icon="tablet_home"
+      style="font-size: 24px;"
+      class="menu_icon"
+    />
+    <kw-btn
+      borderless
+      icon="tablet_task"
+      style="font-size: 24px;"
+      class="menu_icon"
+    />
+    <kw-btn
+      borderless
+      icon="tablet_recently"
+      style="font-size: 24px;"
+      class="menu_icon"
+    />
+    <kw-btn
+      borderless
+      icon="tablet_basket"
+      style="font-size: 24px;"
+      class="menu_icon"
+    />
+    <kw-btn
+      borderless
+      icon="tablet_menu"
+      style="font-size: 24px;"
+      class="menu_icon"
+      @click="openTotalMenu"
+    />
+  </q-footer>
 </template>
 
 <script>
+import { modal } from '../../plugins/modal';
 
+const showTotalMenu = ref(false);
 export default {
   name: 'MobileFooter',
 
   setup() {
-    return {
+    async function openTotalMenu() {
+      if (showTotalMenu.value) return;
+      showTotalMenu.value = true;
+      await modal({
+        component: async () => await import('../../pages/mobile/MobileTotalMenuP.vue'),
+      });
+      showTotalMenu.value = false;
+    }
 
+    return {
+      openTotalMenu,
+      showTotalMenu,
     };
   },
 };
