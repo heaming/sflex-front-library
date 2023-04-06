d77770ab5adbef78656d8e1cc4b758db24871aed
37982 <youngil_park@kyowon.co.kr>
2023-04-06 16:42:08 +0900

    feat : edu 와플용 footer - 모바일  index.js에 추가

--- src/layouts/mobile/index.js
+++ src/layouts/mobile/index.js
@@ -1,5 +1,6 @@
 import MobileLayout from './MobileLayout.vue';
 import MobileFooter from './MobileFooter.vue';
+import MobileEduFooter from './MobileEduFooter.vue';
 import MobileLeftDrawer from './MobileLeftDrawer.vue';
 import MobileStackView from './MobileStackView.vue';
 import MobileFallbackLogin from './MobileFallbackLogin.vue';
@@ -10,4 +11,5 @@ export {
   MobileLeftDrawer,
   MobileStackView,
   MobileFallbackLogin,
+  MobileEduFooter,
 };
