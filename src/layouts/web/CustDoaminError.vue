52d70cffb6a8b184d8e5515f89f4c51680959b6a
김대영 <gs.piit40@kyowon.co.kr>
2023-04-10 08:55:51 +0900

    Release 0.1.440

---
+++ src/layouts/web/CustDoaminError.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+    <div>
+      <div class="text-h2">
+        해당 페이지로는 접근할 수 없습니다.
+      </div>
+      <br>
+      <div class="text-h3">
+        전달받은 url을 확인해주세요.
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+//
+</script>
