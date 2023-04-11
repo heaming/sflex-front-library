ce40fea4b2401cf6b7d2af2a36cfdcff030345e1
mingyu.kim <gs.piit166@kyowon.co.kr>
2023-04-11 15:18:31 +0900

    fix: datepicker rule 변경

--- src/validate/rules/dateRangeMonths.js
+++ src/validate/rules/dateRangeMonths.js
@@ -1,9 +1,14 @@
 import { isEmpty } from 'lodash-es';
-import { date } from 'quasar';
+import dayjs from 'dayjs';
 
 export default {
-  validator: ([from, to], [months]) => isEmpty(from || to)
-    || (date.extractDate(to, 'YYYYMMDD').valueOf()
-      <= date.addToDate(date.extractDate(from, 'YYYYMMDD'), { months }).valueOf()),
+  validator: (([from, to], [months]) => {
+    if (isEmpty(from || to)) return false;
+    if (dayjs(from, 'YYYYMM').format('YYYYMM') === 'Invalid Date' || dayjs(to, 'YYYYMM').format('YYYYMM') === 'Invalid Date') return false;
+    const dateTo = dayjs(to, 'YYYYMM');
+    const fromAfterMonths = dayjs(from, 'YYYYMM').add(months, 'month');
+    if (fromAfterMonths.diff(dateTo, 'month') < 0) return false;
+    return true;
+  }),
   message: 'MSG_VAL_DATE_RANGE_MONTHS',
 };
