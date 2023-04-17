81f9b4a874bb7cb158586cea163373519c57e1be
mingyu.kim <gs.piit166@kyowon.co.kr>
2023-04-17 13:44:01 +0900

    fix: telephone rule 빈 값일때도 통과시키게 수정

--- src/validate/rules/telephone.js
+++ src/validate/rules/telephone.js
@@ -1,6 +1,10 @@
+import { isEmpty } from 'lodash-es';
+
 const regex = /^\d{2,3}-\d{3,4}-\d{4}$/;
 export default {
   validator: (value) => {
+    if (isEmpty(value)) return true;
+
     const stringValue = String(value);
     if (stringValue.indexOf('-') >= 0) return regex.test(value);
 
