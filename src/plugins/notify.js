0d49e5a613f14aa012bed5f8d551c1cab7de99b8
김대영 <gs.piit40@kyowon.co.kr>
2023-04-18 17:21:25 +0900

    fix: notify 에 아이콘 옵션 추가.

--- src/plugins/notify.js
+++ src/plugins/notify.js
@@ -2,8 +2,13 @@ import { GlobalNotifyVmKey } from '../consts/private/symbols';
 import { addGlobalData } from '../utils/private/globalData';
 import { defineGetters } from '../utils/private/globalProperty';
 
-export function notify(message) {
+const normalizeOptions = (options = {}) => ({
+  icon: options.icon,
+});
+
+export function notify(message, options = {}) {
   addGlobalData({
+    ...normalizeOptions(options),
     message,
     vmKey: GlobalNotifyVmKey,
   });
