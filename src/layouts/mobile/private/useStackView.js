6d628a3079a40848adb52d0cb0599e6215e8ce85
mingyu.kim <gs.piit166@kyowon.co.kr>
2023-04-24 09:30:12 +0900

    fix: stackView beforeResolve 추가

--- src/layouts/mobile/private/useStackView.js
+++ src/layouts/mobile/private/useStackView.js
@@ -1,4 +1,4 @@
-import { last, findIndex } from 'lodash-es';
+import { last, findIndex, map } from 'lodash-es';
 
 export default () => {
   const router = useRouter();
@@ -45,8 +45,39 @@ export default () => {
     add(router.currentRoute.value);
   }
 
+  function throwIfInvalidRoute(to, from) {
+    const pageUseIsSub = to.meta.pageUseCode === 'S';
+
+    if (pageUseIsSub) {
+      // case route sub to parents
+      const fromMenuUid = from?.meta.menuUid;
+      const fromMenuPaths = store.getters['meta/getMenuPaths'](fromMenuUid);
+      const fromParentsMenuUids = map(fromMenuPaths, 'key');
+
+      const toMenuUid = to.meta.menuUid;
+      const toIsParents = fromParentsMenuUids.includes(toMenuUid);
+
+      // case route parents to sub
+      const toParentsMenuUid = to.meta.parentsMenuUid;
+      const actualFrom = from?.meta.redirectedFrom || from;
+      const fromIsParents = actualFrom?.meta.menuUid === toParentsMenuUid;
+
+      const isSiblings = to.meta.parentsMenuUid === from.meta.parentsMenuUid;
+
+      if (!toIsParents && !fromIsParents && !isSiblings) {
+        throw new Error(`Navigation aborted to ${to.fullPath}, sub route can only routed from it's parents.`);
+      }
+    }
+  }
+
+  const removeBeforeResolve = router.beforeResolve(async (to, from, next) => {
+    throwIfInvalidRoute(to, from);
+    next();
+  });
+
   onBeforeUnmount(() => {
     removeAfterEach();
+    removeBeforeResolve();
   });
 
   return {
