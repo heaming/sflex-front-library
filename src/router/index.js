d60d0b74a07f52bac8ab3ff3de7ea383041d8104
김대영 <gs.piit40@kyowon.co.kr>
2023-05-10 08:37:14 +0900

    fix: routing 에러 확인 test

--- src/router/index.js
+++ src/router/index.js
@@ -6,11 +6,20 @@ import afterEach from './hooks/afterEach';
 import env from '../consts/private/env';
 import { defineGetters } from '../utils/private/globalProperty';
 import { rebuildRoutes } from './helper';
+import { alert } from '../plugins/dialog';
 
 const router = createRouter({
   scrollBehavior: () => ({ left: 0, top: 0 }),
   routes: [],
   history: createWebHashHistory(env.BASE_URL),
+});
+
+router.onError(async (error, to) => {
+  if (error.message.includes('Failed to fetch dynamically imported module')
+  || error.message.includes('Importing a module script failed')) {
+    await alert('routing 에러가 났습니다. 브라우저 새로고침 해주세요.');
+    console.log(`router.onError ::: ${to.fullPath}`, to);
+  }
 });
 
 export default router;
