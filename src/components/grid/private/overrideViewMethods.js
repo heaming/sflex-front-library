7317d3ed69349339e3b13073e9dbac4d66fb2e18
mingyu.kim <gs.piit166@kyowon.co.kr>
2023-06-10 14:41:46 +0900

    임시 보류

--- src/components/grid/private/overrideViewMethods.js
+++ src/components/grid/private/overrideViewMethods.js
@@ -387,13 +387,13 @@ function setColumnStyleCallback(column) {
   }
 }
 
-function setRowIndicatorStyleCallback(view, data) {
-  view.rowIndicator.cellStyleCallback = (g, type, index) => {
-    if (type === 'indicator' && data.getRowState(view.getDataRow(index)) === 'created') {
-      return 'hide-row-indicator';
-    }
-  };
-}
+// function setRowIndicatorStyleCallback(view, data) {
+//   view.rowIndicator.cellStyleCallback = (g, type, index) => {
+//     if (type === 'indicator' && data.getRowState(vindex) === 'created') {
+//       return 'hide-row-indicator';
+//     }
+//   };
+// }
 
 function normalizeColumn(column, view) {
   const normalizedColumn = cloneDeep(column);
@@ -408,7 +408,7 @@ function normalizeColumn(column, view) {
   setColumnEditor(normalizedColumn, field);
   setColumnCellButton(normalizedColumn, field);
   setColumnStyleCallback(normalizedColumn, field);
-  setRowIndicatorStyleCallback(view, data);
+  // setRowIndicatorStyleCallback(view, data);
   return normalizedColumn;
 }
 
