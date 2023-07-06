322aaeeecd3e9fcc2d3cb3fbd9b926e2e23e25f3
mingyu.kim <gs.piit166@kyowon.co.kr>
2023-07-06 10:13:05 +0900

    fix: grid button boxed 추가

--- dev/src/pages/templates/grid/button/Button.vue
+++ dev/src/pages/templates/grid/button/Button.vue
@@ -129,7 +129,29 @@ function initGrid(data, view) {
     { fieldName: 'col4', width: '100', styleName: 'text-center rg-button-exceldown', renderer: { type: 'button' } },
     { fieldName: 'col5', width: '100', styleName: 'text-center rg-button-exceldown rg-button-disabled', renderer: { type: 'button' } },
     { fieldName: 'col6', width: '100', styleName: 'text-center rg-button-excelup', renderer: { type: 'button' } },
-    { fieldName: 'col7', width: '100', styleName: 'rg-button-icon--search', button: 'action' },
+    { fieldName: 'col7',
+      width: '100',
+      styleName: 'rg-button-icon--search',
+      button: 'action',
+      styleCallback: (g, cell) => {
+        if (cell.index.dataRow % 3 === 0) {
+          return {
+            styleName: 'rg-button-icon--search',
+            editable: true,
+          };
+        }
+
+        if (cell.index.dataRow % 3 === 1) {
+          return {
+            styleName: 'rg-button-icon--search boxed-icon',
+            editable: false,
+          };
+        }
+        return {
+          styleName: 'rg-button-icon--search boxed-icon-disable',
+          editable: false,
+        };
+      } },
     { fieldName: 'col8', width: '100', styleName: 'rg-button-icon--search rg-button-disabled', button: 'action' },
   ];
   data.setFields(fields);
@@ -138,7 +160,12 @@ function initGrid(data, view) {
   view.editOptions.editable = true;
 
   data.setRows([
-    { col1: '기본 활성', col2: '기본 비활성', col3: '링크 언더라인', col4: '엑셀 다운로드', col5: '엑셀 다운 비활성', col6: '엑셀 업로드', col7: '액션 검색', col8: '액션 검색비활성' },
+    { col1: '기본 활성1', col2: '기본 비활성1', col3: '링크 언더라인1', col4: '엑셀 다운로드1', col5: '엑셀 다운 비활성1', col6: '엑셀 업로드1', col7: '액션 검색(편집가능)1', col8: '액션 검색비활성1' },
+    { col1: '기본 활성2', col2: '기본 비활성2', col3: '링크 언더라인2', col4: '엑셀 다운로드2', col5: '엑셀 다운 비활성2', col6: '엑셀 업로드2', col7: '액션 검색(편집불가)2', col8: '액션 검색비활성2' },
+    { col1: '기본 활성3', col2: '기본 비활성3', col3: '링크 언더라인3', col4: '엑셀 다운로드3', col5: '엑셀 다운 비활성3', col6: '엑셀 업로드3', col7: '액션 검색(disable)3', col8: '액션 검색비활성3' },
+    { col1: '기본 활성4', col2: '기본 비활성4', col3: '링크 언더라인4', col4: '엑셀 다운로드4', col5: '엑셀 다운 비활성4', col6: '엑셀 업로드4', col7: '액션 검색4', col8: '액션 검색비활성4' },
+    { col1: '기본 활성5', col2: '기본 비활성5', col3: '링크 언더라인5', col4: '엑셀 다운로드5', col5: '엑셀 다운 비활성5', col6: '엑셀 업로드5', col7: '액션 검색5', col8: '액션 검색비활성5' },
+    { col1: '기본 활성6', col2: '기본 비활성6', col3: '링크 언더라인6', col4: '엑셀 다운로드6', col5: '엑셀 다운 비활성6', col6: '엑셀 업로드6', col7: '액션 검색6', col8: '액션 검색비활성6' },
   ]);
 }
 
