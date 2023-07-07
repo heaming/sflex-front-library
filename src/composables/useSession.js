d78e751b2efd6ca955251e6bfb9dab556202939a
gs.piit40 <gs.piit40@kyowon.co.kr>
2023-07-07 16:17:09 +0900

    fix: sso healthcheck test

--- src/composables/useSession.js
+++ src/composables/useSession.js
@@ -105,7 +105,10 @@ export default () => {
       await initSession();
       await routerIsReady();
     } else if (env.VITE_LOGIN_URL && window.location.origin !== env.VITE_HTTP_CUST_ORIGIN) {
-      locationReplace(env.VITE_LOGIN_URL); // redirect to sso
+      const res = await http.get(env.VITE_SSO_HEALTH_CHECK_URL);
+      if (res.data) {
+        locationReplace(env.VITE_LOGIN_URL); // redirect to sso
+      }
     }
   }
 
