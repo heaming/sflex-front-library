9874f640120efcd9265e08550aebb7220764cc97
mingyu.kim <gs.piit166@kyowon.co.kr>
2023-07-10 14:23:39 +0900

    printUtil 추가

---
+++ src/utils/print.js
@@ -0,0 +1,32 @@
+/**
+ * print Element
+ * @param elementRef
+ */
+function print(printContents) {
+  const app = document.getElementById('app');
+  const printDiv = document.createElement('DIV');
+  printDiv.className = 'print-division';
+  printDiv.innerHTML = printContents;
+  document.body.appendChild(printDiv);
+
+  document.querySelectorAll('.q-dialog').forEach((el) => {
+    el.style.display = 'none';
+  });
+  app.style.display = 'none';
+  window.print();
+
+  document.querySelectorAll('.q-dialog').forEach((el) => {
+    el.style.display = 'block';
+  });
+  app.style.display = 'block';
+  printDiv.style.display = 'none';
+  document.body.removeChild(printDiv);
+}
+export function printElement(elementRef) {
+  print(elementRef.value.$el.innerHTML);
+}
+
+export function printElementsByClassName(className) {
+  const el = document.getElementsByClassName(className)[0];
+  print(el.innerHTML);
+}
