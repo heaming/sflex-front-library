9ecc91d0420a7970a132f6aa1279f2884a175000
gs.piit40 <gs.piit40@kyowon.co.kr>
2023-08-03 16:51:31 +0900

    fix: layout visible 세팅

--- src/utils/grid.js
+++ src/utils/grid.js
@@ -572,6 +572,16 @@ function exportGrid(view, options, onProgress, onComplete) {
   });
 }
 
+function getLayoutVisibleByColumn(layout, columns) {
+  let visible;
+  if (layout.items?.length > 0) {
+    layout.items.forEach((item) => { item.visible = getLayoutVisibleByColumn(item, columns); });
+  } else {
+    visible = columns.find((col) => col._name === layout.column)?.visible;
+  }
+  return visible;
+}
+
 function getLayoutWidthByColumn(layout, columns) {
   let width;
   if (layout.items?.length > 0) {
@@ -594,8 +604,9 @@ export async function exportView(view, options) {
     const columns = view.getColumns();
     options.exportLayout.forEach((layout) => {
       layout.width = getLayoutWidthByColumn(layout, columns);
+
       if (!options.allColumns) {
-        layout.visible = columns.find((col) => col._name === layout.column)?.visible;
+        layout.visible = getLayoutVisibleByColumn(layout, columns);
         // allColumns가 아니고, hideColumns 가 있으면 숨김.
         // allColumns일 경우 hideColumns 무시
         if (options.hideColumns && options.hideColumns.includes(layout.column)) layout.visible = false;
