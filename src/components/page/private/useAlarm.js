b38d41f284463327b707f109e15f877cb5b6f2fc
gs.piit40 <gs.piit40@kyowon.co.kr>
2023-08-11 08:03:36 +0900

    fix: 알람 windowPopup 추가.

--- src/components/page/private/useAlarm.js
+++ src/components/page/private/useAlarm.js
@@ -1,16 +1,23 @@
 import { http } from '../../../plugins/http';
+import { open } from '../../../utils/popup';
 
 export default () => {
   const { push } = useRouter();
   const { dispatch } = useStore();
   async function readAlarm(alarm) {
-    const { linkUrl, readYn } = alarm.value;
+    const { linkUrl, readYn, windowPopupYn } = alarm.value;
 
     if (readYn === 'N') {
       await http.put(`/sflex/common/common/alarm/read/${alarm.value.alarmId}`);
       await dispatch('meta/fetchAlarms');
     }
-    if (linkUrl) push(linkUrl);
+    if (linkUrl) {
+      if (windowPopupYn === 'Y') {
+        open(linkUrl);
+      } else {
+        push(linkUrl);
+      }
+    }
   }
 
   async function readAllAlarm(alarmIds) {
