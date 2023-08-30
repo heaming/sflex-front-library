7405af64e51ed85827d6e0f5e0036478a3581fa2
gs.piit40 <gs.piit40@kyowon.co.kr>
2023-08-30 22:17:45 +0900

    fix: 원복

--- src/layouts/web/WebLayout.vue
+++ src/layouts/web/WebLayout.vue
@@ -27,12 +27,9 @@ export default {
   setup() {
     const { getters, commit } = useStore();
     const { currentRoute } = useRouter();
-    const user = getters['meta/getUserInfo'];
 
     const isAuthenticated = computed(() => getters['meta/isAuthenticated']);
-    const isCustDomain = window.location.origin === env.VITE_HTTP_CUST_ORIGIN || user.userId === 'anonymous';
-    console.log('test HERE &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&');
-    console.log(user.userId);
+    const isCustDomain = window.location.origin === env.VITE_HTTP_CUST_ORIGIN;
 
     const { applicationId, menuUid } = currentRoute.value.meta;
 
