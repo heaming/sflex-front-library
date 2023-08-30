f4a2dbf9dd0f9d831ca6c08b247a11df3112299f
gs.piit40 <gs.piit40@kyowon.co.kr>
2023-08-30 21:01:13 +0900

    fix: prd test

--- config/vite/index.js
+++ config/vite/index.js
@@ -29,17 +29,19 @@ const normalizeConfig = (config = {}) => ({
   plugins: config.plugins || [],
 });
 
-function addQueryPlugin(jsUrls) {
+function addQueryPlugin(urls, cdnUrl) {
   return {
     name: 'add-query-plugin',
     transformIndexHtml(html) {
-      // JavaScript 파일에 쿼리 문자열 추가
+      // grid.css 파일에 쿼리 문자열 추가
       // eslint-disable-next-line no-restricted-syntax
-      for (const url of jsUrls) {
-        const regex = new RegExp(`<link rel="modulepreload" href="${url}">`, 'g');
-        const time = Date.now();
-        const replacement = `<link rel="modulepreload" href="${url}?v=${time}">`; // 여기에서 쿼리 문자열을 추가 또는 수정
-        html = html.replace(regex, replacement);
+      for (const url of urls) {
+        const regex = new RegExp(`<link rel="modulepreload" href="${url}\\.[a-fA-F0-9]{6}\\.js">`, 'g');
+        const match = html.match(regex);
+        if (match) {
+          const replacement = match[0].replace(cdnUrl, '/');
+          html = html.replace(regex, replacement);
+        }
       }
 
       return html;
@@ -76,8 +78,8 @@ exports.defineConfig = (config) => {
           // '/assets/plugin-vue_export-helper.js',
           // `${loadEnv(pluginArgs)?.config()?.define?.__VUE_IMPORT_META_ENV__?.VITE_CDN_ORIGIN}
           // /assets/plugin-vue_export-helper.js`,
-          // `${loadEnv(pluginArgs)?.config()?.define?.__VUE_IMPORT_META_ENV__?.VITE_CDN_ORIGIN}/assets/app.js`,
-        ]),
+          `${loadEnv(pluginArgs)?.config()?.define?.__VUE_IMPORT_META_ENV__?.VITE_CDN_ORIGIN}/assets/grid`,
+        ], loadEnv(pluginArgs)?.config()?.define?.__VUE_IMPORT_META_ENV__?.VITE_CDN_ORIGIN || '/'),
         vue({
           template: { transformAssetUrls },
         }),
