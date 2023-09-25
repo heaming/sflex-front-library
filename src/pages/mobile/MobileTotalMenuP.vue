305310f486c5c7c31168f5d6950b1874e43e053e
gs.piit40 <gs.piit40@kyowon.co.kr>
2023-09-25 09:06:26 +0900

    fix: 삭제된 메뉴 삭제

--- src/pages/mobile/MobileTotalMenuP.vue
+++ src/pages/mobile/MobileTotalMenuP.vue
@@ -402,8 +402,6 @@ async function openUserInfoBottomSheet() {
     items: [
       { value: 'changePassword', label: '비밀번호 변경' },
       { value: 'homeSetting', label: '개인 홈 설정' },
-      { value: 'mobilePrinterInit', label: '모바일프린터 초기화' },
-      { value: 'searchLetter', label: '공문조회' },
       { value: 'sessionChange', label: '세션 변경' },
       { value: 'logout', label: '로그아웃' },
     ],
