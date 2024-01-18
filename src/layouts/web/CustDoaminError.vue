75bc47072fd0a6909e05edde06b261f25ef49cea
gs.piit40 <gs.piit40@kyowon.co.kr>
2024-01-18 17:28:57 +0900

    fix: anonymous 세션 끝난거 테스트

--- src/layouts/web/CustDoaminError.vue
+++ src/layouts/web/CustDoaminError.vue
@@ -2,11 +2,11 @@
   <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
     <div>
       <div class="text-h2">
-        해당 페이지로는 접근할 수 없습니다.
+        해당 페이지로는 접근할 수 없거나, 유효시간이 지났습니다.
       </div>
       <br>
       <div class="text-h3">
-        전달받은 url을 확인해주세요.
+        전달받은 url을 확인 및 재시도 부탁드립니다.
       </div>
     </div>
   </div>
